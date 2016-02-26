9a09e41e5d4b86c56a8277f8fba2ed97d37c29ae
Santiago P De Rosso <spderosso@gmail.com>
2016-02-25 22:47:41 -0500

    moved rxjs add imports to the service

--- patterns/messaging/feed/src/components/shared/feed.ts
+++ patterns/messaging/feed/src/components/shared/feed.ts
@@ -1,6 +1,9 @@
 import {Injectable, Inject} from "angular2/core";
 import {Http} from "angular2/http";
 import {Observable} from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/observable/fromArray";
+import "rxjs/add/operator/mergeMap";
 
 import {Content, Publisher, Name} from "../../shared/data";
 
--- patterns/messaging/feed/src/dev/boot.ts
+++ patterns/messaging/feed/src/dev/boot.ts
@@ -2,10 +2,6 @@ import {provide} from "angular2/core";
 import {bootstrap} from "angular2/platform/browser";
 import {FeedPatternComponent} from "./feed-pattern.component";
 
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/mergeMap";
-import "rxjs/add/observable/fromArray";
-
 
 const APIS = {feed: "http://localhost:3000"};
 
