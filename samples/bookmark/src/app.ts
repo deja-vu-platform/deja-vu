24ab1dc6c0e7593bad8b6f643a597c6871463a99
Santiago P De Rosso <sperezde@csail.mit.edu>
2016-07-21 12:39:34 -0400

    remove unused code

--- samples/bookmark/src/app.ts
+++ samples/bookmark/src/app.ts
@@ -1,78 +1,12 @@
 /// <reference path="../typings/tsd.d.ts" />
-const graphql = require("graphql");
 import {Mean} from "mean-loader";
 import {ServerBus} from "server-bus";
-import {Helpers} from "helpers";
-
-
-const topic_type = new graphql.GraphQLObjectType({
-  name: "Topic",
-  fields: () => ({
-    name: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
-    posts: {"type": new graphql.GraphQLList(topic_type)}
-  })
-});
-
-const post_type = new graphql.GraphQLObjectType({
-  name: "Post",
-  fields: () => ({
-    name: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
-    content: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
-    topics: {
-      "type": new graphql.GraphQLList(topic_type)
-    }
-  })
-});
-
-const user_type = new graphql.GraphQLObjectType({
-  name: "User",
-  fields: () => ({
-    username: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
-    follows: {
-      "type": new graphql.GraphQLList(new graphql.GraphQLUnionType({
-        name: "Follow",
-        types: [user_type, topic_type],
-        resolveType: value => ("username" in value) ? user_type : topic_type
-      }))
-    },
-    posts: {
-      "type": new graphql.GraphQLList(post_type)
-    }
-  })
-});
-
-
-const schema = new graphql.GraphQLSchema({
-  query: new graphql.GraphQLObjectType({
-    name: "Query",
-    fields: {
-      user: {
-        "type": user_type,
-        args: {
-          username: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
-        },
-        resolve: (root, {username}) => {
-          console.log(`getting ${username}`);
-        }
-      },
-      posts: {
-        "type": new graphql.GraphQLList(post_type),
-        resolve: (root) => {
-          console.log("getting posts");
-        }
-      }
-    }
-  })
-});
 
 
 const mean = new Mean();
 const bus = new ServerBus(
     mean.fqelement,  mean.ws, {user: {create: undefined, update: undefined}},
     mean.comp, mean.locs);
-
-
-Helpers.serve_schema(mean.ws, schema);
 
 mean.start();
 
