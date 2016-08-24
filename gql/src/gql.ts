9fd19419b74279543295db8124fde854989aa9bc
Santiago P De Rosso <spderosso@gmail.com>
2016-08-24 17:47:29 -0400

    tmp 3

---
+++ gql/src/gql.ts
@@ -0,0 +1,53 @@
+/// <reference path="../typings/tsd.d.ts" />
+import {Injectable, Inject} from "angular2/core";
+import {Http, Headers} from "angular2/http";
+import {Observable} from "rxjs/observable";
+
+import "rxjs/add/operator/map";
+
+import * as _u from "underscore";
+
+
+@Injectable()
+export class GraphQlService {
+  private _api: string;
+
+  constructor(
+    private _http: Http, @Inject("fqelement") fqelement,
+    @Inject("locs") locs) {
+    this._api = locs[fqelement];
+  }
+
+  post(query): Observable<any> {
+    const headers = new Headers();
+    headers.append("Content-type", "application/json");
+    const query_str = query.replace(/ /g, "");
+
+    return this._http
+      .post(
+          this._api + "/graphql",
+          JSON.stringify({query: "mutation {" + query_str + "}"}),
+          {headers: headers})
+      .map(res => res.json());
+  }
+
+  get(query): Observable<any> {
+    const query_str = query.replace(/ /g, "");
+    return this._http
+      .get(this._api + `/graphql?query=query+{${query_str}}`)
+      .map(res => res.json())
+      .map(json_res => json_res.data);
+  }
+
+  pobj(o: Object) {
+    return "{" + _u
+      .map(
+          _u.filter(_u.keys(o), k => !k.startsWith("_")),
+          k => k + ": \"" + o[k] + "\"")
+      .join(", ") + "}";
+  }
+
+  plist(l: any[]) {
+    return "[" + _u.map(l, i => this.pobj(i)).join(", ") + "]";
+  }
+}
