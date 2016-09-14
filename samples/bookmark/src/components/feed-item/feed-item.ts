fb7400afa1464a739144a763bc014907618f3b46
Santiago P De Rosso <spderosso@gmail.com>
2016-09-14 16:58:53 -0400

    forgot to commit feed-item

---
+++ samples/bookmark/src/components/feed-item/feed-item.ts
@@ -0,0 +1,33 @@
+import {Widget, ClientBus, field} from "client-bus";
+
+import {provide} from "angular2/core";
+
+
+// tmp hack
+@Widget({
+  template: `
+  <div class="list-group">
+    <h4 class="list-group-item-heading">
+      <dv-widget name="Author" fqelement="dv-messaging-post"
+       [fields]="fields">
+      </dv-widget>
+    </h4>
+    <p class="list-group-item-text">
+      {{post.content}}
+    </p>
+    <div class="list-group-item-text">
+      <dv-widget name="Labels" fqelement="dv-organization-label"
+       [fields]="fields">
+      </dv-widget>
+    </div>
+  </div>
+  `,
+  ng2_providers: [provide("fqelement", {useValue: "dv-samples-bookmark"})]
+})
+export class FeedItemComponent {
+  post = {};
+
+  constructor(client_bus: ClientBus) {
+    client_bus.init(this, [field("post", "post")]);
+  }
+}
