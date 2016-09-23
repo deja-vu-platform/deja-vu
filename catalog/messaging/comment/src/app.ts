b3bc8b87da0209ca206b373505bb4b95b4355ea9
Santiago P De Rosso <spderosso@gmail.com>
2016-09-23 14:17:42 -0400

    getting started with grafo

--- catalog/messaging/comment/src/app.ts
+++ catalog/messaging/comment/src/app.ts
@@ -5,6 +5,7 @@ const graphql = require("graphql");
 import {Mean} from "mean-loader";
 import {Helpers} from "helpers";
 import {ServerBus} from "server-bus";
+import {Grafo} from "grafo";
 
 import * as _u from "underscore";
 
@@ -62,86 +63,76 @@ const bus = new ServerBus(
 
 //////////////////////////////////////////////////
 
-const comment_type = new graphql.GraphQLObjectType({
-  name: "Comment",
-  fields: () => ({
-    atom_id: {"type": graphql.GraphQLString},
-    content: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
-    author: {
-      "type": author_type,
-      resolve: comment => mean.db.collection("authors")
-        .findOne({atom_id: comment.author.atom_id})
-    }
-  })
-});
+const grafo = new Grafo(mean.db);
 
-const author_type = new graphql.GraphQLObjectType({
-  name: "author",
-  fields: () => ({
-    atom_id: {"type": graphql.GraphQLString},
-    name: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
+const schema = grafo
+  // Comment
+  .add_type({
+    name: "Comment",
+    fields: () => ({
+      atom_id: {"type": graphql.GraphQLString},
+      content: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
+      author: {"type": "Author"}
+    })
   })
-});
-
-const target_type = new graphql.GraphQLObjectType({
-  name: "Target",
-  fields: () => ({
-    name: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
-    comments: {
-      "type": new graphql.GraphQLList(comment_type),
-      resolve: target => {
-        if (_u.isEmpty(target.comments)) return [];
-        return mean.db.collection("comments")
-          .find({atom_id: {$in: target.comments.map(p => p.atom_id)}})
-          .toArray();
+  // Author
+  .add_type({
+    name: "Author",
+    fields: () => ({
+      atom_id: {"type": graphql.GraphQLString},
+      name: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
+    })
+  })
+  // Target
+  .add_type({
+    name: "Target",
+    fields: () => ({
+      name: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
+      comments: {"type": "[Comment]"},
+      newComment: {
+        "type": "Comment",
+        args: {
+          author: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
+          content: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)}
+        },
+        resolve: (target, {author, content}) => {
+          const atom_id = "foo";
+          return Validation.author_exists(author)
+            .then(author => ({
+               atom_id: atom_id,
+               content: content,
+               author: {atom_id: author.atom_id}
+            }))
+            .then(comment => Promise.all([
+                mean.db.collection("comments").insertOne(comment),
+                mean.db.collection("targets")
+                  .updateOne(
+                    {atom_id: target.atom_id},
+                    {$addToSet: {comments: {atom_id: atom_id}}}),
+                bus.create_atom("Comment", atom_id, comment),
+                bus.update_atom(
+                  "Target", target.atom_id,
+                  {$addToSet: {comments: {atom_id: atom_id}}})
+                ]).then(_ => comment));
+        }
       }
-    },
-    newComment: {
-      "type": comment_type,
-      args: {
-        author: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
-        content: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)}
-      },
-      resolve: (target, {author, content}) => {
-        const atom_id = "foo";
-        return Validation.author_exists(author)
-          .then(author => ({
-             atom_id: atom_id,
-             content: content,
-             author: {atom_id: author.atom_id}
-          }))
-          .then(comment => Promise.all([
-              mean.db.collection("comments").insertOne(comment),
-              mean.db.collection("targets")
-                .updateOne(
-                  {atom_id: target.atom_id},
-                  {$addToSet: {comments: {atom_id: atom_id}}}),
-              bus.create_atom("Comment", atom_id, comment),
-              bus.update_atom(
-                "Target", target.atom_id,
-                {$addToSet: {comments: {atom_id: atom_id}}})
-              ]).then(_ => comment));
-      }
-    }
+    })
   })
-});
-
-
-const schema = new graphql.GraphQLSchema({
-  query: new graphql.GraphQLObjectType({
+  .add_type({
     name: "Query",
     fields: () => ({
       target_by_id: {
-        "type": target_type,
+        "type": "Target",
         args: {
           atom_id: {"type": new graphql.GraphQLNonNull(graphql.GraphQLString)},
         },
         resolve: (root, {atom_id}) => mean.db
           .collection("targets").findOne({atom_id: atom_id})
-      }
+     }
     })
   })
-});
+  .schema();
+
 
 namespace Validation {
   export function author_exists(name) {
---
+++ grafo/src/grafo.ts
@@ -0,0 +1,56 @@
+/// <reference path="../typings/tsd.d.ts" />
+const graphql = require("graphql");
+
+import * as _u from "underscore";
+
+
+export class Grafo {
+  types = {};
+
+  constructor(private db) {}
+
+  add_type(t) {
+    this.types[t.name] = t;
+    return this;
+  }
+
+  schema() {
+    const process_type = (f, f_name) => {
+      if (!_u.isString(f.type)) return f;
+
+      const match = f.type.match(/\[(\w+)\]/);
+      if (match !== null) {
+        const f_type = this.types[match[1]];
+        if (f.resolve === undefined) {
+          f.resolve = src => {
+            const collection = this.db.collection(f_name);
+            const target = src[f_name];
+            if (_u.isEmpty(target)) return [];
+            return collection
+              .find({atom_id: {$in: target.map(t => t.atom_id)}})
+              .toArray();
+          };
+        }
+        f.type = new graphql.GraphQLList(f_type);
+      } else {
+        if (f.resolve === undefined) {
+          f.resolve = src => this.db.collection(f_name + "s")
+            .findOne({atom_id: src[f_name].atom_id});
+        }
+        f.type = this.types[f.type];
+      }
+      return f;
+    };
+
+    this.types = _u.mapObject(this.types, t => {
+      const f = t.fields();
+      t.fields = () => _u.mapObject(f, process_type);
+      return t;
+    });
+    this.types = _u.mapObject(this.types, t => {
+      return new graphql.GraphQLObjectType(t);
+    });
+
+    return new graphql.GraphQLSchema({query: this.types["Query"]});
+  }
+}
