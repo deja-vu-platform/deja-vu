25a55d6ecdeb0523d8c0e8ade6640e439c5b5ccd
Santiago P De Rosso <spderosso@gmail.com>
2016-12-02 13:40:36 -0500

    save weekly events

--- catalog/organization/event/src/app.ts
+++ catalog/organization/event/src/app.ts
@@ -7,6 +7,9 @@ import {Helpers} from "helpers";
 import {ServerBus} from "server-bus";
 import {Grafo} from "grafo";
 
+import * as _u from "underscore";
+
+const uuid = require("uuid");
 
 const mean = new Mean();
 
@@ -16,9 +19,9 @@ const handlers = {
     create: Helpers.resolve_create(mean.db, "event"),
     update: Helpers.resolve_update(mean.db, "event")
   },
-  "weekly_event": {
-    create: Helpers.resolve_create(mean.db, "weekly_event"),
-    update: Helpers.resolve_update(mean.db, "weekly_event")
+  "weeklyevent": {
+    create: Helpers.resolve_create(mean.db, "weeklyevent"),
+    update: Helpers.resolve_update(mean.db, "weeklyevent")
   },
   description: {
     create: Helpers.resolve_create(mean.db, "description"),
@@ -44,9 +47,8 @@ const schema = grafo
     fields: {
       atom_id: {"type": graphql.GraphQLString},
       // todo: grafo should allow weak types
-      date: {"type": graphql.GraphQLString},
-      start_time: {"type": graphql.GraphQLString},
-      end_time: {"type": graphql.GraphQLString}
+      start_date: {"type": graphql.GraphQLString},
+      end_date: {"type": graphql.GraphQLString}
     }
   })
   .add_type({
@@ -67,7 +69,38 @@ const schema = grafo
       guests: {"type": new graphql.GraphQLList(graphql.GraphQLString)}
     },
     resolve: (_, {starts_on, ends_on, start_time, end_time, guests}) => {
-      return undefined;
+      const starts_on_date = new Date(starts_on);
+      const ends_on_date = new Date(ends_on);
+
+      const inserts = [];
+      const event_ids = [];
+      for (
+        let event_date = starts_on_date; event_date <= ends_on_date;
+        event_date.setDate(event_date.getDate() + 7)) {
+        const start_date = new Date(event_date.toString());
+        start_date.setTime(start_time);
+
+        const end_date = new Date(event_date.toString());
+        end_date.setTime(end_time);
+
+        console.log(event_date.toString());
+        const eid = uuid.v4();
+        event_ids.push(eid);
+        inserts.push(
+            mean.db.collection("events")
+              .insertOne({
+                atom_id: eid,
+                start_date: start_date.toString(),
+                end_date: end_date.toString()
+              }));
+      }
+      const weekly_event = {
+        atom_id: uuid.v4,
+        events: _u.map(event_ids, eid => ({atom_id: eid}))
+      };
+      return Promise.all(inserts)
+        .then(_ => mean.db.collection("weeklyevents").insertOne(weekly_event))
+        .then(_ => weekly_event);
     }
   })
   .add_type({
@@ -87,4 +120,4 @@ const schema = grafo
 
 Helpers.serve_schema(mean.ws, schema);
 
-mean.start();
+grafo.init().then(_ => mean.start());
