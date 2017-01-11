ec84b013e15bf0b19e33ebed44446ece435a5de0
Santiago P De Rosso <spderosso@gmail.com>
2017-01-10 20:38:51 -0500

    more updates

--- core/mean-loader/src/dev/boot.template.ts
+++ core/mean-loader/src/dev/boot.template.ts
@@ -1,66 +1,50 @@
-import {Component, provide, PLATFORM_DIRECTIVES} from "angular2/core";
-import {bootstrap} from "angular2/platform/browser";
-import {ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES}
-from "angular2/router";
+import {NgModule, Component} from "@angular/core";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {BrowserModule} from "@angular/platform-browser";
+import {RouterModule, Routes} from "@angular/router";
 
-import {ClientBus} from "client-bus";
+import {WidgetLoader, ClientBus} from "client-bus";
 
 
 @@wid_imports
 
-const COMP_INFO = @@comp_info;
-const WCOMP_INFO = @@wcomp_info;
-const NAME = "@@name";
 const MODE = "@@mode";
-const LOCS = @@locs;
 
 const WID_NAMES = @@wid_names;
 
-
-let providers = [];
-
-providers = providers.concat(
-  Object.keys(LOCS)
-    .map(cliche_i => {
-      const cliche_i_split = cliche_i.split("-");
-      let name = cliche_i_split[2];
-      if (cliche_i_split.length === 4) {
-        name = name + "-" + cliche_i_split[3];
-      }
-      return provide(name + ".api", {useValue: LOCS[cliche_i]});
-    }));
-
-providers = providers.concat([
-    ROUTER_PROVIDERS,
-    provide("fqelement", {useValue: NAME}),
-    provide("locs", {useValue: LOCS}),
-    provide("CompInfo", {useValue: COMP_INFO}),
-    provide("WCompInfo", {useValue: WCOMP_INFO}),
-    ClientBus
-    ]);
+const appRoutes: Routes = @@route_config;
 
 
 let template;
-let directives;
 if (MODE === "dev") {
   template = `
-     This is the development page, widgets: {{wid_names}}
+     This is the development page, widgets: ${WID_NAMES}
      <div class="container">
        @@wid_selectors
      </div>
   `;
-  directives = @@wid_directives;
 } else {
   template = `<router-outlet></router-outlet>`;
-  directives = [ROUTER_DIRECTIVES];
 }
 
 @Component({
   selector: "root",
   template: template,
-  directives: directives
+  providers: [
+    {provide: "fqelement", useValue: "@@name"},
+    {provide: "CompInfo", useValue: @@comp_info},
+    {provide: "WCompInfo", useValue: @@wcomp_info},
+    {provide: "locs", useValue: @@locs},
+    ClientBus]
 })
-@@route_config
 class RootComponent {}
-  
-bootstrap(RootComponent, providers);
+
+
+@NgModule({
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  declarations: [RootComponent, WidgetLoader],
+  bootstrap: [RootComponent]
+})
+export class RootModule {}
+
+platformBrowserDynamic().bootstrapModule(RootModule);
