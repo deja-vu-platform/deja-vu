4a98745acd088f4fd94805352e9015d3741b0e29
Santiago P De Rosso <spderosso@gmail.com>
2017-01-19 19:11:49 -0500

    parse used cliches

--- core/lang/src/lang.ts
+++ core/lang/src/lang.ts
@@ -5,6 +5,32 @@ import * as path from "path";
 
 const grammar_path = path.join(__dirname, "grammar.ohm");
 const grammar = ohm.grammar(fs.readFileSync(grammar_path, "utf-8"));
+const semantics = grammar.createSemantics()
+  .addOperation("comp", {
+  })
+  .addOperation("wcomp", {
+  })
+  .addOperation("routeInfo", {
+  })
+  .addOperation("usedCliches", {
+    ClicheDecl: (cliche, name, uses, key1, para, key2) => {
+      const ret = {};
+      const uses_used_cliches = uses.usedCliches()[0];
+      if (uses_used_cliches !== undefined) {
+        uses_used_cliches.forEach(c => {
+          ret[c] = (ret[c] === undefined) ? 1 : ret[c] + 1;
+        });
+      }
+      return ret;
+    },
+    ClicheUsesDecl: (uses, name1, asDecl1, comma, name2, asDecl2) => []
+      .concat(name1.usedCliches()).concat(name2.usedCliches()),
+    usedClicheName: (category, slash, name) => {
+      return `dv-${category.sourceString}-${name.sourceString.toLowerCase()}`;
+    }
+  })
+  .addOperation("usedWidgets", {
+  });
 // console.log(grammar);
 debug_match("../../catalog/messaging/post/post.dv");
 debug_match("../../samples/morg/morg.dv");
@@ -19,5 +45,7 @@ function debug_match(fp) {
   if (r.failed()) {
     console.log(r.message);
     // console.log(grammar.trace(dv).toString());
+  } else {
+    console.log(semantics(r).usedCliches());
   }
 }
