03de38b98ffbf8002ca0649f9f75468da3766d23
Santiago P De Rosso <spderosso@gmail.com>
2017-02-11 12:34:29 -0500

    rm unused field in landing

--- samples/bookmark/src/components/landing/landing.ts
+++ samples/bookmark/src/components/landing/landing.ts
@@ -3,15 +3,10 @@ import {Widget, ClientBus, field} from "client-bus";
 
 @Widget({fqelement: "dv-samples-bookmark"})
 export class LandingComponent {
-  signin_user;
   route: {value: string};
 
   constructor(client_bus: ClientBus) {
-    client_bus.init(this, [
-      field("signin_user", "User"),
-      field("register_user", "User"),
-      field("route", "Text")
-      ]);
+    client_bus.init(this, [field("route", "Text")]);
     this.route.value = "/app/home";
   }
 }
