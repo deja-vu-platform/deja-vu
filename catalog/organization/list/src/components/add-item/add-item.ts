eaf6892eb6115ceba4c491cf0be1553a4e518cbc
Stacy Ho <stacyho@mit.edu>
2017-03-21 19:30:06 -0400

    working list cliche

---
+++ catalog/organization/list/src/components/add-item/add-item.ts
@@ -0,0 +1,33 @@
+import {GraphQlService} from "gql";
+
+import {Widget} from "client-bus";
+
+
+@Widget({
+  fqelement: "dv-organization-list",
+  ng2_providers: [GraphQlService]
+})
+export class AddItemComponent {
+  item = {atom_id: "", name: ""};
+  list = {atom_id: ""};
+  submit_ok = {value: false};
+
+  constructor(private _graphQlService: GraphQlService) {}
+
+  onSubmit() {
+    if (!this.list.atom_id) return;
+
+    this._graphQlService
+      .get(`
+        list_by_id(atom_id: "${this.list.atom_id}") {
+          addItem(name: "${this.item.name}") {
+            atom_id
+          }
+        }
+      `)
+      .subscribe(atom_id => {
+        this.item.atom_id = atom_id;
+        this.submit_ok.value = true;
+      });
+  }
+}
---
+++ catalog/organization/list/src/components/show-list/show-list.ts
@@ -0,0 +1,63 @@
+import {GraphQlService} from "gql";
+
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/from";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/mergeMap";
+
+import {Widget, ClientBus} from "client-bus";
+
+import * as _u from "underscore";
+
+
+export interface Item {
+  atom_id: string;
+  name: string;
+}
+
+
+@Widget({
+  fqelement: "dv-organization-list",
+  ng2_providers: [GraphQlService]
+})
+export class ShowListComponent {
+  list = {atom_id: "", items: [], on_change: _ => undefined};
+  fields = {};
+
+  constructor(
+      private _graphQlService: GraphQlService, private _clientBus: ClientBus) {}
+
+  dvAfterInit() {
+    const update_list = () => {
+      if (!this.list.atom_id) return;
+
+      this.list.items = [];
+      return this._graphQlService
+        .get(`
+          list_by_id(atom_id: "${this.list.atom_id}") {
+            items {
+              atom_id,
+              name
+            }
+          }
+        `)
+        .map(data => data.list_by_id.items)
+        .flatMap((items, unused_ix) => Observable.from(items))
+        .map((item: Item) => {
+          const item_atom: Item = this._clientBus.new_atom("Item");
+          item_atom.atom_id = item.atom_id;
+          item_atom.name = item.name;
+          return {item: item_atom};
+        })
+        .map(item => _u.extend(item, this.fields))
+        .subscribe(item => {
+          this.list.items.push(item);
+        });
+    };
+
+    update_list();
+    this.list.on_change(update_list);
+  }
+
+
+}
