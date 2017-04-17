766a55e50ce503b8ea315b225c6a484051bdd177
Santiago P De Rosso <spderosso@gmail.com>
2017-04-17 11:01:25 -0400

    update task

--- catalog/community/task/gruntfile.js
+++ catalog/community/task/gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
-  require("mean-loader").GruntTask.task(
+  require("mean-loader").GruntTask.cliche_task(
       grunt,
-      "dv-organization-task",
-      [{name: "CreateTask"}, {name: "ShowTasks"}]);
+      "Task",
+      ["CreateTask", "ShowTasks"]);
 }
