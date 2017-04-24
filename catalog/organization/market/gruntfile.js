11d36aeca76317a567802d43df76ffc2138ff460
Santiago P De Rosso <spderosso@gmail.com>
2017-04-24 18:42:07 -0400

    update gruntfiles

--- catalog/organization/market/gruntfile.js
+++ catalog/organization/market/gruntfile.js
@@ -1,8 +1,8 @@
 module.exports = function(grunt) {
-  require("mean-loader").GruntTask.task(
+  require("mean-loader").GruntTask.cliche_task(
       grunt,
-      "dv-organization-market",
-      [{name: "AffordTable"}, {name: "CreateGood"}, {name: "AddAmount"}, 
-        {name: "ShowBalance"}, {name: "BuyGood"}, {name: "BuyGoodAtFractionOfPrice"},
-        {name: "ShowMyGoods"}, {name: "ShowGoodsToBuy"}]);
+      "Market",
+      ["AffordTable", "CreateGood", "AddAmount", 
+       "ShowBalance", "BuyGood", "BuyGoodAtFractionOfPrice",
+       "ShowMyGoods", "ShowGoodsToBuy"]);
 }
--- catalog/organization/task/gruntfile.js
+++ catalog/organization/task/gruntfile.js
@@ -1,8 +1,8 @@
 module.exports = function(grunt) {
   require("mean-loader").GruntTask.task(
       grunt,
-      "dv-organization-task",
-      [{name: "CreateTask"}, {name: "ShowUncompletedTasks"},
-      {name: "ShowUnapprovedTasks"}, {name: "ShowApprovedTasks"}, 
-      {name: "ShowAssignedTasks"}, {name: "ShowPendingApprovalTasks"}]);
+      "Task",
+      ["CreateTask", "ShowUncompletedTasks",
+       "ShowUnapprovedTasks", "ShowApprovedTasks", 
+       "ShowAssignedTasks", "ShowPendingApprovalTasks"]);
 }
