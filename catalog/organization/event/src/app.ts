1b65ee77319cc5c14ecc446a0fe77cd0921ec8b6
Barry Anthony McNamara III <barryam3@mit.edu>
2017-06-15 14:45:26 -0400

    New singular event cliche for party creation.

--- catalog/organization/event/src/app.ts
+++ catalog/organization/event/src/app.ts
@@ -45,9 +45,9 @@ const schema = grafo
     name: "Event",
     fields: {
       atom_id: {"type": graphql.GraphQLString},
-      // todo: grafo should allow weak types
+      // TODO: grafo should allow weak types
       start_date: {"type": graphql.GraphQLString},
-      end_date: {"type": graphql.GraphQLString}
+      end_date: {"type": graphql.GraphQLString},
     }
   })
   .add_type({
@@ -124,6 +124,49 @@ const schema = grafo
           .insertOne(weekly_event)
           .then(_ => bus.create_atom("WeeklyEvent", weid, weekly_event))
           .then(_ => weekly_event));
+    }
+  })
+  .add_mutation({
+    name: "newPublicEvent",
+    "type": "Event",
+    args: {
+      starts_on: {"type": graphql.GraphQLString},
+      ends_on: {"type": graphql.GraphQLString},
+      start_time: {"type": graphql.GraphQLString},
+      end_time: {"type": graphql.GraphQLString}
+    },
+    resolve: (_, {starts_on, ends_on, start_time, end_time}) => {
+      const get_hh_mm = hh_mm_time => {
+        const hh_mm = hh_mm_time.slice(0, -2).split(":");
+        if (hh_mm_time.slice(-2) === "PM") {
+          hh_mm[0] = Number(hh_mm[0]) + 12;
+        }
+        return hh_mm;
+      }
+
+      const starts_on_date = new Date(starts_on);
+      const ends_on_date = new Date(ends_on);
+
+      const inserts = [];
+      const event_ids = [];
+
+      const start_hh_mm = get_hh_mm(start_time)
+      starts_on_date.setHours(start_hh_mm[0], start_hh_mm[1])
+
+      const end_hh_mm = get_hh_mm(end_time)
+      ends_on_date.setHours(end_hh_mm[0], end_hh_mm[1])
+
+      const eid = uuid.v4();
+      event_ids.push(eid);
+      const e = {
+        atom_id: eid,
+        start_date: starts_on_date.toString(),
+        end_date: ends_on_date.toString()
+      };
+      return mean.db.collection("events")
+        .insertOne(e)
+        .then(_ => bus.create_atom("Event", eid, e));
+        // returns a promise
     }
   })
   .add_type({
---
+++ catalog/organization/event/src/components/show-events/show-events.ts
@@ -0,0 +1,64 @@
+import {ElementRef} from "@angular/core";
+
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/observable/from";
+import "rxjs/add/operator/mergeMap";
+
+import {GraphQlService} from "gql";
+
+import {Widget, ClientBus, field} from "client-bus";
+
+import * as _u from "underscore";
+
+export interface Event {
+  start_date: Date;
+  end_date: Date;
+}
+
+export interface EventItem {
+  event: Event;
+}
+
+@Widget({
+  fqelement: "Event",
+  ng2_providers: [GraphQlService],
+  external_styles: [
+    `https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/` +
+    `bootstrap-select.min.css`
+  ]
+})
+export class ShowEventsComponent {
+  events: EventItem[] = [];
+
+  constructor(
+      private _graphQlService: GraphQlService,
+      private _elementRef: ElementRef,
+      private _clientBus: ClientBus) {
+  }
+
+  dvAfterInit() {
+    this._graphQlService
+      .get(`
+        event_all {
+          atom_id,
+          start_date,
+          end_date
+        }
+      `)
+      .map(data => data.event_all)
+      .flatMap((events: Event[], unused_ix) => Observable
+          .from(events))
+      .map(event => _u
+          .extendOwn(this._clientBus.new_atom("Event"), event))
+      .subscribe(event => this.events.push(event));
+  }
+
+  ngAfterViewInit() {
+    const s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = `https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/` +
+      `1.12.1/js/bootstrap-select.min.js`;
+    this._elementRef.nativeElement.appendChild(s);
+  }
+}
