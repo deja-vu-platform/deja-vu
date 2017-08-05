71f28bcc322f6c6cc358396edbff8e747901751b
Santiago P De Rosso <spderosso@gmail.com>
2017-08-05 16:16:03 -0400

    prevent error in ShowTransaction when good is undefined

--- catalog/community/market/src/components/show-transaction/show-transaction.ts
+++ catalog/community/market/src/components/show-transaction/show-transaction.ts
@@ -8,17 +8,20 @@ import "rxjs/add/operator/map";
 
 @Widget({
   fqelement: "Market",
-  template: `x{{transaction.quantity}} {{transaction.good.name}} ` +
+  template: `x{{transaction.quantity}} {{good_name}} ` +
   `@{{transaction.price}}`,
   ng2_providers: [GraphQlService]
 })
 export class ShowTransactionComponent {
   @Field("Transaction") transaction: TransactionAtom;
+  // Use good_name in template instead of transaction.good.name so that it
+  // doesn't crash if good is undefined
+  good_name = "";
 
   constructor(private _graphQlService: GraphQlService) {}
 
   dvAfterInit() {
-    if (!this.transaction.atom_id || this.transaction.good.name) {
+    if (!this.transaction.atom_id || !this.transaction.good) {
       return;
     }
 
@@ -45,6 +48,7 @@ export class ShowTransactionComponent {
           .map(data => data.good_by_id.name)
           .subscribe(name => {
             this.transaction.good.name = name;
+            this.good_name = name;
             this.transaction.price = transaction_by_id.price;
             this.transaction.quantity = transaction_by_id.quantity;
           });
