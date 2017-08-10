a8d9c398382594423ce281d3784a70e7352b7ea8
Barry Anthony McNamara III <bamazap@hotmail.com>
2017-08-10 14:26:07 -0400

    Additional widgets for showing groups & members

---
+++ catalog/organization/group/src/components/show-members-of-group/show-members-of-group.ts
@@ -0,0 +1,36 @@
+import {Widget, ClientBus, Field} from "client-bus";
+import {GraphQlService} from "gql";
+
+import {MemberAtom, GroupAtom} from "../_shared/data";
+import GroupService from "../_shared/group.service";
+
+
+@Widget({
+  fqelement: "Group",
+  ng2_providers: [
+    GraphQlService,
+    GroupService
+  ]
+})
+export class ShowMembersOfGroupComponent {
+  @Field("Group") group: GroupAtom;
+
+  constructor(
+    private _groupService: GroupService,
+    private _clientBus: ClientBus
+  ) {}
+
+  dvAfterInit() {
+    if (this.group.atom_id) {
+      this._groupService.getMembersOfGroup(this.group.atom_id)
+        .then(members => {
+          this.group.members = members.map((m) => {
+            const member_atom = this._clientBus.new_atom<MemberAtom>("Member");
+            member_atom.atom_id = m.atom_id;
+            member_atom.name = m.name;
+            return member_atom;
+          });
+        });
+    }
+  }
+}
