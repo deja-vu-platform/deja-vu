e7d64bad64cfb4a16ba5e4ae2ebc8fb905f6df54
Barry Anthony McNamara III <bamazap@hotmail.com>
2017-08-23 13:52:43 -0400

    Add disclaimer about legacy modules.

--- catalog/organization/label/src/components/_shared/select2.ts
+++ catalog/organization/label/src/components/_shared/select2.ts
@@ -3,24 +3,29 @@ import {} from "@types/select2";
 
 import {loadScript, loadStylesheet, waitFor} from "./utils";
 
-const scriptSrc = "https://cdn.jsdelivr.net/select2/4.0.3/js/select2.min.js";
-const stylePath = "https://cdn.jsdelivr.net/select2/4.0.3/css/select2.min.css";
+// Note: Using legacy modules which cannot be imported
+// Re-do with angular packages once this is possible
 
 // Instance of a Select2 box
 // (jQuery replacement for select box -- https://select2.github.io/)
 // Warning: make sure Select2.loadAPI() has resolved before instantiating
 export default class Select2 {
+  private static scriptSrc = 
+    "https://cdn.jsdelivr.net/select2/4.0.3/js/select2.min.js";
+  private static stylePath =
+    "https://cdn.jsdelivr.net/select2/4.0.3/css/select2.min.css";
+
   $select: any; // jQuery object
 
   static loadAPI(): Promise<{}> {
     return waitFor(window, "jQuery")
       .then(_ => {
         // insert tags to load API and styles from CDN
-        if (!window[scriptSrc] && !window["jQuery"].fn.select2) {
-          loadScript(scriptSrc);
+        if (!window[Select2.scriptSrc] && !window["jQuery"].fn.select2) {
+          loadScript(Select2.scriptSrc);
         }
-        if (!window[stylePath]) {
-          loadStylesheet(stylePath);
+        if (!window[Select2.stylePath]) {
+          loadStylesheet(Select2.stylePath);
         }
         return waitFor(window["jQuery"].fn, "select2");
       });
