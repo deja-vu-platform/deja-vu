e1ac78630e631d47c851585e06b2211bf857a493
Barry Anthony McNamara III <bamazap@hotmail.com>
2017-08-25 15:02:25 -0400

    Creation widgets for follow

--- catalog/community/follow/src/components/_shared/utils.ts
+++ catalog/community/follow/src/components/_shared/utils.ts
@@ -5,3 +5,19 @@ export function doesFollow(follower: Follower, publisher: Publisher): boolean {
     return followed.atom_id === publisher.atom_id;
   }).length;
 }
+
+// allows references like `foo.bar.baz` to be done without fear of error
+// returns objct[flds[0]][flds[1]]...
+// or dflt if a field access is attempted on null or undefined
+export function getOrDefault<T>(objct: object, flds: string[], dflt: T): T {
+  let broke = false;
+  let obj: any = objct;
+  flds.forEach((fld) => {
+    if (obj !== null && obj !== undefined) {
+      obj = obj[fld];
+    } else {
+      broke = true;
+    }
+  });
+  return (broke ? dflt : obj);
+}
---
+++ catalog/community/follow/src/components/new-message-button/new-follower-button.ts
@@ -0,0 +1,44 @@
+import {Widget, Field, PrimitiveAtom} from "client-bus";
+import {GraphQlService} from "gql";
+
+import {MessageAtom} from "../_shared/data";
+import FollowService from "../_shared/follow.service";
+
+
+@Widget({
+  fqelement: "Follow",
+  ng2_providers: [
+    GraphQlService,
+    FollowService
+  ]
+})
+export class NewFollowerButtonComponent {
+  @Field("Message") message : MessageAtom;
+  @Field("boolean") submit_ok: PrimitiveAtom<boolean>;
+
+  failMsg: string;
+
+  constructor(private _followService: FollowService) {}
+
+  dvAfterInit() {
+    this.submit_ok.on_after_change(() => {
+      if (this.submit_ok.value) {
+        this.submit_ok.value = false;
+        this.message.atom_id = "";
+      }
+    });
+  }
+
+  submit() {
+    this._followService.createFollower()
+      .then(atom_id => {
+        if (atom_id) {
+          this.message.atom_id = atom_id;
+          this.submit_ok.value = true;
+          this.failMsg = "";
+        } else {
+          this.failMsg = "Failed to create message.";
+        }
+      });
+  }
+}
