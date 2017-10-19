eca3094542497c14f49577259f58239508345c6c
Czarina Lao <mcslao@mit.edu>
2017-10-19 02:06:38 -0400

    Make create profile widget

---
+++ catalog/community/profile/src/components/create-profile-with-redirect/create-profile-with-redirect.ts
@@ -0,0 +1,44 @@
+import {GraphQlService} from "gql";
+
+import {ClientBus, Widget, Field, PrimitiveAtom, WidgetValue} from "client-bus";
+import {ProfileAtom} from "../shared/data";
+
+
+@Widget({fqelement: "Profile", ng2_providers: [GraphQlService]})
+export class CreateProfileWithRedirectComponent {
+  @Field("Profile") profile: ProfileAtom;
+  @Field("Widget") on_create_ok: PrimitiveAtom<WidgetValue>;
+
+  username_error = false;
+
+  constructor(
+    private _graphQlService: GraphQlService,
+    private _client_bus: ClientBus) {}
+
+  onSubmit() {
+    // clear previous error
+    this.username_error = false;
+
+    this._graphQlService
+      .post(`
+        createProfile(
+          username: "${this.profile.username}",
+          first_name: "${this.profile.first_name}",
+          last_name: "${this.profile.last_name}",
+          email: "${this.profile.email}",
+          phone: "${this.profile.phone}",
+          birthday: "${this.profile.birthday}"
+        )
+        {
+          atom_id
+        }
+      `)
+      .subscribe(atom_id => {
+        this.profile.atom_id = atom_id;
+        this._client_bus.navigate(this.on_create_ok.value);
+      },
+      err => {
+        this.username_error = true;
+      });
+  }
+}
