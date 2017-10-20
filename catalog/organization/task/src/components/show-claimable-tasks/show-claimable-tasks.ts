ed6860fc1670f13b356bc2c6c2497ffaa33e9251
Czarina Lao <mcslao@mit.edu>
2017-10-20 02:33:50 -0400

    Create show claimable tasks widget

---
+++ catalog/organization/task/src/components/show-claimable-tasks/show-claimable-tasks.ts
@@ -0,0 +1,41 @@
+import {GraphQlService} from "gql";
+
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/from";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/mergeMap";
+
+import {Widget, ClientBus, Field, Atom, AfterInit} from "client-bus";
+import {NamedAtom} from "../shared/data";
+
+
+@Widget({fqelement: "Task", ng2_providers: [GraphQlService]})
+export class ShowCLaimableTasksComponent implements AfterInit {
+  @Field("Assigner") assigner: Atom;
+  claimableTasks = [];
+
+  constructor(
+    private _graphQlService: GraphQlService, private _clientBus: ClientBus) {}
+
+  dvAfterInit() {
+      if (this.assigner.atom_id === undefined) return;
+
+      this._graphQlService
+      .get(`
+        claimableTasks(assigner_id: "${this.assigner.atom_id}"){
+          name
+        }
+      `)
+      .map(data => data.assignedTasks)
+      .flatMap((tasks, unused_ix) => Observable.from(tasks))
+      .map((task: NamedAtom) => {
+        const task_atom = this._clientBus.new_atom<NamedAtom>("Task");
+        task_atom.atom_id = task.atom_id;
+        task_atom.name = task.name;
+        return task;
+      })
+      .subscribe(task => {
+        this.claimableTasks.push(task);
+      });
+  }
+}
