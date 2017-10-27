0ae50af8ca386886167aa6cfc3dcde23963506df
Czarina Lao <mcslao@mit.edu>
2017-10-27 13:35:29 -0400

    Create cancel compound transaction widget

---
+++ catalog/community/market/src/components/cancel-compound-transaction-button/cancel-compound-transaction-button.ts
@@ -0,0 +1,27 @@
+import {GraphQlService} from "gql";
+import {Widget, Field} from "client-bus";
+import {CompoundTransactionAtom} from "../../shared/data";
+
+@Widget({
+  fqelement: "Market",
+  ng2_providers: [GraphQlService]
+})
+export class CancelCompoundTransactionButtonComponent {
+  @Field("CompoundTransaction") compoundTransaction: CompoundTransactionAtom;
+
+  constructor(private _graphQlService: GraphQlService) {}
+
+  cancelCompoundTransaction() {
+    if (!this.compoundTransaction.atom_id) return;
+
+    this._graphQlService
+      .post(`
+        CancelCompoundTransaction(
+          compound_transaction_id: "${this.compoundTransaction.atom_id}"
+        )
+      `)
+      .subscribe(_ => {
+        this.compoundTransaction = undefined;
+      });
+  }
+}
