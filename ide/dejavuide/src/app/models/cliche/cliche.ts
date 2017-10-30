0c7ef28fd2bd4d94ee2b2e22add187654e3a91d3
Shinjini Saha <shinjini@mit.edu>
2017-10-30 15:49:54 -0400

    updating the representation so that cliches now contain all their widgets; will have to move the getter methods from widgets to cliches?

--- ide/dejavuide/src/app/models/cliche/cliche.ts
+++ ide/dejavuide/src/app/models/cliche/cliche.ts
@@ -38,11 +38,14 @@ export class UserCliche extends Cliche {
     for (const widgetId of Object.keys(object.widgets.pages)){
       uc.addPage(Widget.fromObject(object.widgets.pages[widgetId]));
     }
+    for (const widgetId of Object.keys(object.widgets.used)){
+      uc.addUsedWidget(Widget.fromObject(object.widgets.used[widgetId]));
+    }
     for (const widgetId of Object.keys(object.widgets.unused)){
-      uc.addUnusedWidget(Widget.fromObject(object.widgets.pages[widgetId]));
+      uc.addUnusedWidget(Widget.fromObject(object.widgets.unused[widgetId]));
     }
     for (const widgetId of Object.keys(object.widgets.templates)){
-      uc.addTemplate(Widget.fromObject(object.widgets.pages[widgetId]));
+      uc.addTemplate(Widget.fromObject(object.widgets.templates[widgetId]));
     }
     return uc;
   }
@@ -58,6 +61,7 @@ export class UserCliche extends Cliche {
     };
     this.widgets = new Map<string, Map<string, Widget>>();
     this.widgets.set('pages', new Map<string, Widget>());
+    this.widgets.set('used', new Map<string, Widget>());
     this.widgets.set('unused', new Map<string, Widget>());
     this.widgets.set('templates', new Map<string, Widget>());
   }
@@ -98,10 +102,6 @@ export class UserCliche extends Cliche {
     return Array.from(this.widgets.get('templates').keys());
   }
 
-  /**
-  * makes a new unused widget
-  * @param widget
-  */
   addUnusedWidget (widget: Widget) {
     this.widgets.get('unused').set(widget.getId(), widget);
   }
@@ -111,7 +111,19 @@ export class UserCliche extends Cliche {
   };
 
   getUnusedWidgetIds (): string[] {
-    return  Array.from(this.widgets.get('unused').keys());
+    return  Array.from(this.widgets.get('used').keys());
+  }
+
+  addUsedWidget (widget: Widget) {
+    this.widgets.get('used').set(widget.getId(), widget);
+  }
+
+  removeUsedWidget = function(widgetId){
+    this.widgets.get('used').delete(widgetId);
+  };
+
+  getUsedWidgetIds (): string[] {
+    return  Array.from(this.widgets.get('used').keys());
   }
 }
 
