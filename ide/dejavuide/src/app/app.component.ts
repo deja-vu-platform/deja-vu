e99736c94eb9584d5d9e3d1bcb548406534628d0
Shinjini Saha <shinjini@mit.edu>
2017-11-16 17:25:08 -0500

    merging complete

--- ide/dejavuide/src/app/app.component.ts
+++ ide/dejavuide/src/app/app.component.ts
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
 
 import { Component } from '@angular/core';
 
-import { Project } from './models/project/project';
-import { RouterService, PageType } from './services/router.service';
-
-=======
-import { Component } from '@angular/core';
-
->>>>>>> 7b0b57b86363049aee602e5f90346efa90de77f1
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  PageType = PageType;
-  selectedPage = PageType.PROJECT_EXPLORER;
-
-  constructor(private routerService: RouterService) {}
-=======
->>>>>>> 7b0b57b86363049aee602e5f90346efa90de77f1
 }
