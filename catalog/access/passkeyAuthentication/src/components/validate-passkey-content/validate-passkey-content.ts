9fc3152b61875fe5b73f8c0efb5fce3a31ca985b
Maryam Archie <marchie@mit.edu>
2017-11-19 21:26:39 -0500

    Split validatepasskeywithredirect into two components but need to refresh to redirect

---
+++ catalog/access/passkeyAuthentication/src/components/validate-passkey-content/validate-passkey-content.ts
@@ -0,0 +1,41 @@
+import { NgClass } from "@angular/common";
+import { Widget, Field, PrimitiveAtom, AfterInit } from "client-bus";
+import { GraphQlService } from "gql";
+import { PasskeyAtom } from "../shared/data";
+
+@Widget({
+    fqelement: "PasskeyAuthentication",
+    ng2_providers: [GraphQlService],
+    ng2_directives: [NgClass]
+})
+export class ValidatePasskeyContentComponent implements AfterInit {
+    @Field("Passkey") passkey: PasskeyAtom;
+    @Field("boolean") submit_ok: PrimitiveAtom<boolean>;
+
+    error = false;
+
+    constructor(private _graphQlService: GraphQlService) { }
+
+    dvAfterInit() {
+        this.submit_ok.on_change(() => {
+            this._graphQlService
+            .post(`
+                validatePasskey(
+                    code: "${this.passkey.code}"
+                )
+            `)
+            .map(data => JSON.parse(data.validatePasskey))
+            .subscribe(
+                token => {
+                    const authenticationToken = token.token;
+                    const authenticatedPasskey = token.passkey;
+                    localStorage.setItem("id_token", authenticationToken);
+                    localStorage.setItem("atom_id",
+                                         authenticatedPasskey.atom_id);
+                },
+                err => {
+                    this.error = true;
+                });
+        });
+    }
+}
