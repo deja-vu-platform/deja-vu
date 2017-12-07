97277f19268dea662c0c73914df9aa23aaafe2ee
Maryam Archie <marchie@mit.edu>
2017-12-07 15:51:30 -0500

    Create CreateTaskForAllAssignees widget
    Fetch assinger atom before showing tasks

--- catalog/organization/task/src/app.ts
+++ catalog/organization/task/src/app.ts
@@ -1,10 +1,10 @@
 const graphql = require("graphql");
-import {Promise} from "es6-promise";
+import { Promise } from "es6-promise";
 
-import {Mean} from "mean-loader";
-import {Helpers} from "helpers";
-import {ServerBus} from "server-bus";
-import {Grafo} from "grafo";
+import { Mean } from "mean-loader";
+import { Helpers } from "helpers";
+import { ServerBus } from "server-bus";
+import { Grafo } from "grafo";
 
 const uuid = require("uuid");
 
@@ -26,7 +26,7 @@ const handlers = {
 };
 
 const bus = new ServerBus(
-    mean.fqelement, mean.ws, handlers, mean.comp, mean.locs);
+  mean.fqelement, mean.ws, handlers, mean.comp, mean.locs);
 
 
 /////////////////////
@@ -37,45 +37,45 @@ const schema = grafo
   .add_type({
     name: "Task",
     fields: {
-      atom_id: {"type": graphql.GraphQLString},
-      name: {"type": graphql.GraphQLString},
-      assigner: {"type": "Assigner"} ,
-      assignee: {"type": "Assignee"},
-      expiration_date: {"type": graphql.GraphQLString},
-      completed: {"type": graphql.GraphQLBoolean},
-      approved: {"type": graphql.GraphQLBoolean},
+      atom_id: { "type": graphql.GraphQLString },
+      name: { "type": graphql.GraphQLString },
+      assigner: { "type": "Assigner" },
+      assignee: { "type": "Assignee" },
+      expiration_date: { "type": graphql.GraphQLString },
+      completed: { "type": graphql.GraphQLBoolean },
+      approved: { "type": graphql.GraphQLBoolean },
     }
   })
   .add_type({
     name: "Assigner",
     fields: {
-      atom_id: {"type": graphql.GraphQLString},
-      name: {"type": graphql.GraphQLString},
+      atom_id: { "type": graphql.GraphQLString },
+      name: { "type": graphql.GraphQLString },
     }
   })
   .add_type({
     name: "Assignee",
     fields: {
-      atom_id: {"type": graphql.GraphQLString},
-      name: {"type": graphql.GraphQLString},
+      atom_id: { "type": graphql.GraphQLString },
+      name: { "type": graphql.GraphQLString },
     }
   })
- .add_mutation({
+  .add_mutation({
     name: "createTask",
     "type": "Task",
     args: {
-      name: {"type": graphql.GraphQLString},
-      assigner_id: {"type": graphql.GraphQLString} ,
-      assignee_id: {"type": graphql.GraphQLString},
-      expires_on: {"type": graphql.GraphQLString}
+      name: { "type": graphql.GraphQLString },
+      assigner_id: { "type": graphql.GraphQLString },
+      assignee_id: { "type": graphql.GraphQLString },
+      expires_on: { "type": graphql.GraphQLString }
     },
-    resolve: (_, {name, assigner_id, assignee_id, expires_on}) => {
+    resolve: (_, { name, assigner_id, assignee_id, expires_on }) => {
       const expiration_date = new Date(expires_on);
       const task = {
         atom_id: uuid.v4(),
         name: name,
-        assigner: {atom_id: assigner_id},
-        assignee: {atom_id: assignee_id},
+        assigner: { atom_id: assigner_id },
+        assignee: { atom_id: assignee_id },
         expiration_date: expiration_date,
         completed: false,
         approved: false
@@ -84,62 +84,102 @@ const schema = grafo
         .all([
           mean.db.collection("tasks").insertOne(task),
           bus.create_atom("Task", task.atom_id, task)
-          ])
+        ])
         .then(_ => task)
-      } 
+    }
   })
- .add_mutation({
+  .add_mutation({
     name: "completeTask",
     "type": graphql.GraphQLBoolean,
     args: {
-      task_id: {"type": graphql.GraphQLString}
+      task_id: { "type": graphql.GraphQLString }
     },
-    resolve: (_, {task_id}) => {
-      const update_op = {$set: {completed: true}};
+    resolve: (_, { task_id }) => {
+      const update_op = { $set: { completed: true } };
       return mean.db.collection("tasks")
-        .updateOne({atom_id: task_id}, update_op)
+        .updateOne({ atom_id: task_id }, update_op)
         .then(_ => bus.update_atom("Task", task_id, update_op))
         .then(_ => true);
-      }
+    }
   })
- .add_mutation({
+  .add_mutation({
     name: "claimTask",
     "type": graphql.GraphQLBoolean,
     args: {
-      task_id: {"type": graphql.GraphQLString},
-      assignee_id: {"type": graphql.GraphQLString}
+      task_id: { "type": graphql.GraphQLString },
+      assignee_id: { "type": graphql.GraphQLString }
     },
-    resolve: (_, {task_id, assignee_id}) => {
-      const update_op = {$set: {"assignee.atom_id": assignee_id}};
+    resolve: (_, { task_id, assignee_id }) => {
+      const update_op = { $set: { "assignee.atom_id": assignee_id } };
       return mean.db.collection("tasks")
-        .updateOne({atom_id: task_id}, update_op)
+        .updateOne({ atom_id: task_id }, update_op)
         .then(_ => bus.update_atom("Task", task_id, update_op))
         .then(_ => true);
-      }
+    }
   })
- .add_mutation({
+  .add_mutation({
     name: "approveTask",
     "type": graphql.GraphQLBoolean,
     args: {
-      task_id: {"type": graphql.GraphQLString}
+      task_id: { "type": graphql.GraphQLString }
     },
-    resolve: (_, {task_id}) => {
-      const update_op = {$set: {approved: true}};
+    resolve: (_, { task_id }) => {
+      const update_op = { $set: { approved: true } };
       return mean.db.collection("tasks")
-        .updateOne({atom_id: task_id}, update_op)
+        .updateOne({ atom_id: task_id }, update_op)
         .then(_ => bus.update_atom("Task", task_id, update_op))
         .then(_ => true);
-      }
-  })
-  .add_query({
+    }
+  }).add_mutation({
+    name: "createTaskForAllAssignees",
+    type: "[Task]",
+    args: {
+      name: { "type": graphql.GraphQLString },
+      assigner_id: { "type": graphql.GraphQLString },
+      expires_on: { "type": graphql.GraphQLString }
+    },
+    resolve: (_, { name, assigner_id, expires_on }) => {
+      console.log("i'm creating things as " + assigner_id);
+      return mean.db.collection("assignees").find({})
+        .toArray()
+        .then(assignee_all => {
+          console.log("all the assignees")
+          console.log(assignee_all);
+          const tasks = assignee_all.map((assignee) => {
+            return {
+              atom_id: uuid.v4(),
+              name: name,
+              assigner: { atom_id: assigner_id },
+              assignee: { atom_id: assignee.atom_id },
+              expiration_date: expires_on,
+              completed: false,
+              approved: false
+            };
+          });
+          return tasks;
+        })
+        .then(tasks => {
+          console.log("i'm going to add the tasks now");
+          return mean.db.collection("tasks").insert(tasks)
+            .then(_ => {
+              tasks.forEach(task => {
+                bus.create_atom("Task", task.atom_id, task)
+              });
+            })
+            .then(_ => tasks)
+        });
+    }
+  }).add_query({
     name: "uncompletedTasks",
     type: "[Task]",
     args: {
-      assignee_id: {"type": graphql.GraphQLString}
+      assignee_id: { "type": graphql.GraphQLString }
     },
-    resolve: (root, {assignee_id}) => {
-      return mean.db.collection("tasks").find({ $and: 
-        [{"assignee.atom_id": assignee_id}, {completed: false}] })
+    resolve: (root, { assignee_id }) => {
+      return mean.db.collection("tasks").find({
+        $and:
+          [{ "assignee.atom_id": assignee_id }, { completed: false }]
+      })
         .toArray();
     }
   })
@@ -147,23 +187,27 @@ const schema = grafo
     name: "unapprovedTasks",
     type: "[Task]",
     args: {
-      assignee_id: {"type": graphql.GraphQLString}
+      assignee_id: { "type": graphql.GraphQLString }
     },
-    resolve: (root, {assignee_id}) => {
-      return mean.db.collection("tasks").find({ $and: 
-        [{"assignee.atom_id": assignee_id}, {completed: true},
-        {approved: false}] }).toArray();
+    resolve: (root, { assignee_id }) => {
+      return mean.db.collection("tasks").find({
+        $and:
+          [{ "assignee.atom_id": assignee_id }, { completed: true },
+          { approved: false }]
+      }).toArray();
     }
   })
   .add_query({
     name: "approvedTasks",
     type: "[Task]",
     args: {
-      assignee_id: {"type": graphql.GraphQLString}
+      assignee_id: { "type": graphql.GraphQLString }
     },
-    resolve: (root, {assignee_id}) => {
-      return mean.db.collection("tasks").find({ $and: 
-        [{"assignee.atom_id": assignee_id}, {approved: true}] })
+    resolve: (root, { assignee_id }) => {
+      return mean.db.collection("tasks").find({
+        $and:
+          [{ "assignee.atom_id": assignee_id }, { approved: true }]
+      })
         .toArray();
     }
   })
@@ -171,24 +215,27 @@ const schema = grafo
     name: "assignedTasks",
     type: "[Task]",
     args: {
-      assigner_id: {"type": graphql.GraphQLString}
+      assigner_id: { "type": graphql.GraphQLString }
     },
-    resolve: (root, {assigner_id}) => {
-      return mean.db.collection("tasks").find({ $and: 
-        [{"assigner.atom_id": assigner_id}, 
-        {completed: false}] }).toArray();
+    resolve: (root, { assigner_id }) => {
+      console.log("here's the assigned tasks");
+      return mean.db.collection("tasks").find({
+        $and:
+          [{ "assigner.atom_id": assigner_id },
+          { completed: false }]
+      }).toArray();
     }
   })
   .add_query({
     name: "claimableTasks",
     type: "[Task]",
     args: {
-      assigner_id: {"type": graphql.GraphQLString}
+      assigner_id: { "type": graphql.GraphQLString }
     },
-    resolve: (root, {assigner_id}) => {
-      return mean.db.collection("tasks").find({ 
-        "assigner.atom_id": {$ne: assigner_id},
-        $and: [{"assignee.atom_id": null}, {completed: false}] 
+    resolve: (root, { assigner_id }) => {
+      return mean.db.collection("tasks").find({
+        "assigner.atom_id": { $ne: assigner_id },
+        $and: [{ "assignee.atom_id": null }, { completed: false }]
       }).toArray();
     }
   })
@@ -196,36 +243,43 @@ const schema = grafo
     name: "pendingApprovalTasks",
     type: "[Task]",
     args: {
-      assigner_id: {"type": graphql.GraphQLString}
+      assigner_id: { "type": graphql.GraphQLString }
     },
-    resolve: (root, {assigner_id}) => {
-      return mean.db.collection("tasks").find({ $and: 
-        [{"assigner.atom_id": assigner_id}, {completed: true},
-        {approved: false}] }).toArray();
+    resolve: (root, { assigner_id }) => {
+      return mean.db.collection("tasks").find({
+        $and:
+          [{ "assigner.atom_id": assigner_id }, { completed: true },
+          { approved: false }]
+      }).toArray();
     }
   })
   .schema();
-
 
 Helpers.serve_schema(mean.ws, schema);
 
 grafo.init().then(_ => {
   if (mean.debug) {
     mean.db.collection("tasks").insertMany([
-      {atom_id: "1", name: "Eat", completed: false, approved: false,
-      assigner: {atom_id: "10"}, assignee: {atom_id: "11"}},
-      {atom_id: "2", name: "Sleep", completed: true, approved: false,
-      assigner: {atom_id: "10"}, assignee: {atom_id: "11"}},
-      {atom_id: "3", name: "Work", completed: true, approved: true,
-      assigner: {atom_id: "10"}, assignee: {atom_id: "11"}}],
+      {
+        atom_id: "1", name: "Eat", completed: false, approved: false,
+        assigner: { atom_id: "10" }, assignee: { atom_id: "11" }
+      },
+      {
+        atom_id: "2", name: "Sleep", completed: true, approved: false,
+        assigner: { atom_id: "10" }, assignee: { atom_id: "11" }
+      },
+      {
+        atom_id: "3", name: "Work", completed: true, approved: true,
+        assigner: { atom_id: "10" }, assignee: { atom_id: "11" }
+      }],
       (err, res) => { if (err) throw err; });
 
     mean.db.collection("assigners").insertOne(
-      {name: "Bob", atom_id: "10"},
+      { name: "Bob", atom_id: "10" },
       (err, res) => { if (err) throw err; });
 
     mean.db.collection("assignees").insertOne(
-      {name: "Joe", atom_id: "11"},
+      { name: "Joe", atom_id: "11" },
       (err, res) => { if (err) throw err; });
 
   }
