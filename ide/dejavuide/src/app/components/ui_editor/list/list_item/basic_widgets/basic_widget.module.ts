e204785206ba6a9bed2f29eb25c289a2ef66dc94
Shinjini Saha <shinjini@mit.edu>
2017-12-11 20:12:35 -0500

    starting on basic widget list components

---
+++ ide/dejavuide/src/app/components/ui_editor/list/list_item/basic_widgets/basic_widget.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+  ],
+  entryComponents: [
+  ],
+  providers: [],
+  exports: [
+  ]
+})
+export class BasicWidgetListItemModule { }
---
+++ ide/dejavuide/src/app/components/ui_editor/list/list_item/basic_widgets/link.component.ts
@@ -0,0 +1,192 @@
+
+import { Component, Input, AfterViewInit, ElementRef, ViewChild} from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { Cliche } from '../../../../../models/cliche/cliche';
+import { BaseWidget } from '../../../../../models/widget/widget';
+
+// List item needs drag-and-drop
+import * as jQuery from 'jquery';
+import 'jquery-ui-dist/jquery-ui';
+
+const $ = <any>jQuery;
+
+@Component({
+  selector: 'dv-list-item',
+  templateUrl: './list_item.component.html',
+  styleUrls: ['./list_item.component.css']
+})
+export class ListItemComponent implements AfterViewInit {
+  @Input() isDraggable = false;
+  @Input() isDeletable = true;
+  @Input() widget: Widget;
+  @ViewChild('ghost', {read: ElementRef}) ghost: ElementRef;
+  dragging = false;
+  innerShown = false;
+  el: HTMLElement;
+
+  constructor(
+    el: ElementRef,
+    public dialog: MatDialog,
+    private projectService: ProjectService
+  ) {
+    this.el = el.nativeElement;
+  }
+
+  ngAfterViewInit() {
+    if (this.isDraggable) {
+      $(this.el).draggable(
+        { opacity: 1,
+          revert: 'invalid',
+          cursorAt: {top: 0, left: 0},
+          helper: (e, ui) => {
+              const widgetContainer = $(this.ghost.nativeElement).clone();
+              widgetContainer.css({
+                display: 'block',
+                visibility: 'visible',
+              });
+              return widgetContainer;
+          },
+          appendTo: '.edit',
+          cursor: '-webkit-grabbing',
+          scroll: true,
+          snap: '.grid-cell, .grid-x, .grid-y',
+          snapTolerance: 10,
+          start: () => {
+              // $('.grid').css({
+              //     visibility: 'visible'
+              // });
+              // $('.grid-line').css({
+              //     visibility: 'hidden'
+              // });
+              this.dragging = true;
+          },
+          drag: (event, ui) => {
+              // grid.detectGridLines(ui.helper);
+          },
+          stop: (event, ui) => {
+              // $('.grid').css({
+              //     visibility: 'hidden'
+              // });
+              // $('.grid-line').css({
+              //     visibility: 'hidden'
+              // });
+              // var widgetId = draggingWidget.meta.id;
+              // var isNewWidget = $(ui.helper).data('newcomponent');
+              // if (!isNewWidget) {
+              //     var widgetContainerOld = $('#'+containerRef+'_' + widgetId + '_old');
+              //     if (!$(ui.helper).data('dropped')) {// not properly dropped!
+              //         widgetContainerOld.attr('id', containerRef+'_' + widgetId);
+              //         widgetContainerOld.css({
+              //             opacity: 1,
+              //         });
+              //     } else { // properly dropped
+              //         widgetContainerOld.remove();
+              //     }
+              // } else {
+              //     delete userApp.widgets.unused[widgetId];
+              //     $("#user-components-list").find("[data-componentid='" + widgetId + "']").remove();
+              // }
+              // listDisplay.refresh();
+              this.dragging = false;
+          }
+      });
+    }
+  }
+
+  handleDelete(): void {
+    if (this.isDeletable) {
+      const dialogRef = this.dialog.open(DeleteDialogComponent, {
+        width: '250px',
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.deleteUserWidget();
+        }
+      });
+    }
+  }
+
+  toggleShow() {
+    this.innerShown = !this.innerShown;
+  }
+
+  private deleteUserWidget() {
+    console.log('clicked delete');
+    // if (userApp.getNumWidgets() == 1){
+    //     return; //don't delete the last one TODO is the the right way to go?
+    // }
+    // userApp.deleteWidget(userWidgetId);
+    // var workSurfaceRef = workSurface.getWorkSurfaceRef();
+
+    // $('#'+workSurfaceRef+'_'+userWidgetId).remove();
+    // $('#disabled_'+userWidgetId+'_'+workSurfaceRef+'_'+userWidgetId).remove(); // also remove disabled ones
+
+    // if (userWidgetId == selectedUserWidget.meta.id){ // strings will also do
+    //     var otherIds = userApp.getAllWidgetIds();
+    //     selectedUserWidget = userApp.getWidget(otherIds[0]);
+    //     workSurface.loadUserWidget(selectedUserWidget, currentZoom);
+    // }
+    // if (userWidgetId == userApp.widgets.indexId){
+    //     userApp.widgets.indexId = null;
+    // }
+    // listDisplay.refresh();
+  }
+
+  // that.select = function(id){
+  //     $('.selected').removeClass("selected");
+  //     $("[data-componentid='" + id + "']").addClass('selected');
+  // };
+}
+
+
+// $('.components').on('click', '.component-name-container', function () {
+//   // Save the current values
+//   var oldState = {zoom : currentZoom};
+//   var workSurfaceRef = workSurface.getWorkSurfaceRef();
+//   $('#'+workSurfaceRef+'_'+selectedUserWidget.meta.id).data('state', oldState);
+//   dragAndDrop.registerWidgetDragHandleDraggable();
+//   var widgetId = $(this).closest('.widget').data('componentid');
+//   listDisplay.select(widgetId);
+//   selectedUserWidget = userApp.getWidget(widgetId);
+//   listDisplay.updateDraggables(selectedUserWidget);
+//   workSurface.loadUserWidget(selectedUserWidget);
+//   style.setUpStyleColors(selectedUserWidget);
+//   $('#outer-container').scrollTop(0); // TODO DRY
+//   $('#outer-container').scrollLeft(0);
+
+// });
+
+// $('.components').on('dblclick', '.component-name', function (e) {
+//   var newNameInputElt = $($(this).parent().find('.new-name-input'));
+//   var submitRenameElt = $($(this).parent().find('.submit-rename'));
+//   newNameInputElt.val($(this).text());
+//   submitRenameElt.removeClass('not-displayed');
+//   $(this).addClass('not-displayed');
+//   newNameInputElt.focus();
+//   newNameInputElt.select();
+// });
+
+// $('.components').on('keypress', '.new-name-input', function (event) {
+//   if (event.which == 13) {
+//       event.preventDefault();
+//       var widgetListElt = $(this).closest('.widget');
+//       var widgetId = widgetListElt.data('componentid');
+//       var widgetNameElt = widgetListElt.find('.component-name');
+//       var submitRenameElt = widgetListElt.closest('.widget').find('.submit-rename');
+
+//       widgetNameElt.removeClass('not-displayed');
+//       submitRenameElt.addClass('not-displayed');
+//       var newName = $(this).val();
+//       if (newName.length === 0) { // empty string entered, don't change the name!
+//           return;
+//       }
+//       widgetNameElt.text(newName);
+//       $('.component-options .component-name').text(newName);
+
+//       var widget = userApp.getWidget(widgetId);
+//       widget.meta.name = newName;
+//   }
+// });
+
