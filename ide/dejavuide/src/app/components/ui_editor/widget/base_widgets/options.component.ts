08f128870fe8b0e107234033d8b07ec67a6c1bb2
Shinjini Saha <shinjini@mit.edu>
2017-12-20 14:13:02 -0500

    added some event handlers to the options component

--- ide/dejavuide/src/app/components/ui_editor/widget/base_widgets/options.component.ts
+++ ide/dejavuide/src/app/components/ui_editor/widget/base_widgets/options.component.ts
@@ -6,4 +6,37 @@ import { Component, Input } from '@angular/core';
 })
 export class WidgetOptionsComponent {
   @Input() editDisabled = false;
+
+  clearStyles() {
+    console.log('clear styles clicked');
+  }
+
+  showTooltip() {
+    console.log('show tooltip clicked');
+    // container.find('.tooltip').addClass('open');
+  }
+
+  createTemplate() {
+    console.log('create template clicked');
+    // var copy = createUserWidgetCopy(widget);
+    // userApp.addTemplate(copy);
+    // listDisplay.refresh();
+  }
+
+  delete() {
+    console.log('delete clicked');
+  }
+
+  unlink() {
+    console.log('unlink clicked');
+  }
+
+  moveUp() {
+    console.log('move down clicked');
+  }
+
+  moveDown() {
+    console.log('move down clicked');
+  }
+
 }
