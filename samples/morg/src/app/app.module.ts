7e49f5fae5cd94178dc9fa2db8505fe50f0ac011
Santiago P De Rosso <spderosso@gmail.com>
2018-02-14 02:48:05 -0500

    start with morg migration

---
+++ samples/morg/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { GATEWAY_URL, DvModule } from 'dv-core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShowGroupMeetingComponent } from './show-group-meeting/show-group-meeting.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ShowGroupMeetingComponent
+  ],
+  imports: [
+    BrowserModule,
+    DvModule
+  ],
+  providers: [{
+    provide: GATEWAY_URL, useValue: 'http://localhost:3000/api'
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ samples/morg/src/app/show-group-meeting/show-group-meeting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'morg-show-group-meeting',
+  templateUrl: './show-group-meeting.component.html',
+  styleUrls: ['./show-group-meeting.component.css']
+})
+export class ShowGroupMeetingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
