7df7a83e22528a0cbba9c903137a9a4f007ff313
Santiago P De Rosso <spderosso@gmail.com>
2018-02-27 00:52:26 -0500

    fix new-weekly-event

--- packages/catalog/event/src/app/event/new-weekly-event/new-weekly-event.component.ts
+++ packages/catalog/event/src/app/event/new-weekly-event/new-weekly-event.component.ts
@@ -23,7 +23,7 @@ export class NewWeeklyEventComponent {
       .post('/graphql', JSON.stringify({
         query: `mutation {
           createWeeklyEvent(input: {
-            id: "${this.id}",
+            ${this.id ? `id: "${this.id}",` : ''}
             startsOn: "${this.startsOn}", endsOn: "${this.endsOn}",
             startTime: "${this.startTime}", endTime: "${this.endTime}"
           }) {
@@ -38,38 +38,5 @@ export class NewWeeklyEventComponent {
         this.startTime = '';
         this.endTime = '';
       });
-  }
-
-  /**
-   * Fix an inconsistency with the current time appearing in time boxes when
-   * they are clicked. The inconsistency is that when a user clicks the time
-   * control for the first time, the current (rounded) time appears.
-   * Subsequently, after the time is cleared, this doesn't happen anymore.
-   */
-  timeClickHandler(event: Event) {
-    const MINUTE_ROUNDING_FACTOR = 15;
-    const FIRST_PM_HOUR = 12;
-
-    if (!event.srcElement['value']) {
-      // Get a string containing the current time
-      const currentTime = new Date();
-
-      // Note: The behavior of the control is a bit weird in that it rounds up
-      // to the nearest 15 minutes. We need to do that to keep consistent
-      let minutes: number = currentTime.getMinutes();
-      if (minutes % MINUTE_ROUNDING_FACTOR !== 0) {
-        // Pushing the time forward wraps properly
-        currentTime.setMinutes(minutes
-          + (MINUTE_ROUNDING_FACTOR
-            - (minutes % MINUTE_ROUNDING_FACTOR)));
-        minutes = currentTime.getMinutes();
-      }
-
-      const totalHours: number = currentTime.getHours();
-      const actualHours: number = totalHours % FIRST_PM_HOUR;
-      const amPm: string = totalHours >= FIRST_PM_HOUR ? 'PM' : 'AM';
-      event.srcElement['value'] = actualHours.toString() + ':'
-        + minutes.toString() + ' ' + amPm;
-    }
   }
 }
