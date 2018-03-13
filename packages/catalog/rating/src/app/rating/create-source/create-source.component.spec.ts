0dea07360006fce2b5b65e3ad647948616c67b49
Santiago P De Rosso <spderosso@gmail.com>
2018-03-13 19:48:50 -0400

    lint rating

--- packages/catalog/rating/src/app/rating/create-source/create-source.component.spec.ts
+++ packages/catalog/rating/src/app/rating/create-source/create-source.component.spec.ts
@@ -20,6 +20,7 @@ describe('CreateSourceComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component)
+      .toBeTruthy();
   });
 });
--- packages/catalog/rating/src/app/rating/create-target/create-target.component.spec.ts
+++ packages/catalog/rating/src/app/rating/create-target/create-target.component.spec.ts
@@ -20,6 +20,7 @@ describe('CreateTargetComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component)
+      .toBeTruthy();
   });
 });
