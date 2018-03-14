a72c2aecd5487ed7fb0769307392965da25a79f2
Maryam Archie <marchie@mit.edu>
2018-03-14 13:18:00 -0400

    create DeleteResource widget for authorization cliche

---
+++ packages/catalog/authorization/src/app/authorization/delete-resource/delete-resource.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component, ElementRef, Input, OnInit
+} from '@angular/core';
+import {
+  GatewayService, GatewayServiceFactory, OnRun, RunService
+} from 'dv-core';
+
+import * as _ from 'lodash';
+
+
+@Component({
+  selector: 'authorization-delete-resource',
+  templateUrl: './delete-resource.component.html',
+  styleUrls: ['./delete-resource.component.css']
+})
+export class DeleteResourceComponent implements OnInit, OnRun {
+  @Input() id;
+
+  resourceDeleted = false;
+  resourceDeletedError: string;
+
+  private gs: GatewayService;
+
+  constructor(
+    private elem: ElementRef, private gsf: GatewayServiceFactory,
+    private rs: RunService) { }
+
+  ngOnInit() {
+    this.gs = this.gsf.for(this.elem);
+    this.rs.register(this.elem, this);
+  }
+
+  deleteEvent() {
+    this.rs.run(this.elem);
+  }
+
+  dvOnRun() {
+    this.gs
+      .post('/graphql', {
+        query: `mutation {
+          deleteResource (id: "${this.id}")
+        }`
+      })
+      .toPromise();
+  }
+}
+
