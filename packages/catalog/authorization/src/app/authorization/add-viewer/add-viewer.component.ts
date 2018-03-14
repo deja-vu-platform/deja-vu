6d34a1942e32eb977fec77c50a7160ffa86e7499
Maryam Archie <marchie@mit.edu>
2018-03-14 13:45:46 -0400

    create AddViewer widget to authorization cliche

---
+++ packages/catalog/authorization/src/app/authorization/add-viewer/add-viewer.component.ts
@@ -0,0 +1,72 @@
+import {
+  Component, ElementRef, EventEmitter,
+  Input, OnInit, Output
+} from '@angular/core';
+import {
+  GatewayService, GatewayServiceFactory, OnAfterAbort,
+  OnAfterCommit, OnRun, RunService
+} from 'dv-core';
+
+import * as _ from 'lodash';
+
+import { Principal, Resource } from '../../../../shared/data';
+
+const SAVED_MSG_TIMEOUT = 3000;
+
+@Component({
+  selector: 'authorization-add-viewer',
+  templateUrl: './add-viewer.component.html',
+  styleUrls: ['./add-viewer.component.css']
+})
+export class AddViewerComponent implements
+  OnInit, OnRun, OnAfterCommit, OnAfterAbort {
+  // Presentation Inputs
+  @Input() id: string;
+  @Input() viewerId: string;
+  @Input() resourceLabel = 'Resource Id';
+  @Input() viewerLabel = 'Viewer Id';
+  @Input() buttonLabel = 'Add Viewer to Resource';
+  @Input() viewerAddedSuccessText = 'Viewer added to resource';
+
+  viewerAddedSuccess = false;
+  viewerAddedErrorText: string;
+
+  private gs: GatewayService;
+
+  constructor(
+    private elem: ElementRef, private gsf: GatewayServiceFactory,
+    private rs: RunService) { }
+
+  ngOnInit() {
+    this.gs = this.gsf.for(this.elem);
+    this.rs.register(this.elem, this);
+  }
+
+  onSubmit() {
+    this.rs.run(this.elem);
+  }
+
+  dvOnRun() {
+    this.gs
+      .post('/graphql', {
+        query: `mutation {
+          addViewerToResource (
+            id: "${this.id}",
+            viewerId: "${this.viewerId}"
+          )
+        }`
+      })
+      .toPromise();
+  }
+
+  dvOnAfterCommit() {
+    this.viewerAddedSuccess = true;
+    window.setTimeout(() => {
+      this.viewerAddedSuccess = false;
+    }, SAVED_MSG_TIMEOUT);
+  }
+
+  dvOnAfterAbort(reason: Error) {
+    this.viewerAddedErrorText = reason.message;
+  }
+}
