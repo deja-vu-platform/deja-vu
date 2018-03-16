672f9fdeeabe378d351402e01427fd8b7bfea320
Maryam Archie <marchie@mit.edu>
2018-03-15 23:10:50 -0400

    import components in app.module

--- packages/catalog/authorization/src/app/authorization/authorization.module.ts
+++ packages/catalog/authorization/src/app/authorization/authorization.module.ts
@@ -1,10 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  MatButtonModule, MatFormFieldModule, MatInputModule,
+  MatSelectModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DvModule } from 'dv-core';
+
+import {
+  AddViewerComponent
+} from './add-viewer/add-viewer.component';
+import {
+  CreatePrincipalComponent
+} from './create-principal/create-principal.component';
+import {
+  CreateResourceComponent
+} from './create-resource/create-resource.component';
+import {
+  DeleteResourceComponent
+} from './delete-resource/delete-resource.component';
+
+const allComponents = [
+  AddViewerComponent, CreatePrincipalComponent,
+  CreateResourceComponent, DeleteResourceComponent
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    BrowserAnimationsModule,
+    CommonModule,
+    DvModule,
+    FormsModule,
+    MatButtonModule, MatInputModule, MatFormFieldModule, MatSelectModule
   ],
-  declarations: []
+  declarations: allComponents,
+  entryComponents: allComponents,
+  exports: allComponents
 })
 export class AuthorizationModule { }
