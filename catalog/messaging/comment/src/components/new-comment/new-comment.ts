2d79dea63d713a07c65c4ef5491ce8257f21c0dc
Maryam Archie <marchie@mit.edu>
2018-04-08 15:26:42 -0400

    delete old version

--- catalog/messaging/comment/src/components/new-comment/new-comment.ts
+++
@@ -1,9 +0,0 @@
-import {Widget, Field} from "client-bus";
-import {AuthorAtom, CommentAtom, TargetAtom} from "../../shared/data";
-
-@Widget({fqelement: "Comment"})
-export class NewCommentComponent {
-  @Field("Author") author: AuthorAtom;
-  @Field("Comment") comment: CommentAtom;
-  @Field("Target") target: TargetAtom;
-}
