75ea42e58fdc673ff3b03a65754be8ccc449425e
Santiago P De Rosso <spderosso@gmail.com>
2018-04-09 16:50:04 -0400

    reuse nav-bar + some style fixes in chorestar

---
+++ samples/chorestar/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component, EventEmitter, OnInit, Output, Input
+} from '@angular/core';
+
+@Component({
+  selector: 'chorestar-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+  @Input() for: string;
+  @Output() loggedInUser = new EventEmitter();
+  user;
+
+  outputAsLoggedInUser(user) {
+    this.loggedInUser.emit(user);
+  }
+}
