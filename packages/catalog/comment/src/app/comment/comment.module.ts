3861672d7947edf35336354188225a16f5bc2487
Maryam Archie <marchie@mit.edu>
2018-04-08 22:45:28 -0400

    lint comment cliche

--- packages/catalog/comment/src/app/comment/comment.module.ts
+++ packages/catalog/comment/src/app/comment/comment.module.ts
@@ -1,18 +1,47 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule,
+  MatSelectModule
+} from '@angular/material';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DvModule } from 'dv-core';
+
+import { CreateAuthorComponent } from './create-author/create-author.component';
+import {
+  CreateCommentComponent
+} from './create-comment/create-comment.component';
+import { CreateTargetComponent } from './create-target/create-target.component';
 import { EditCommentComponent } from './edit-comment/edit-comment.component';
-import { NewCommentComponent } from './new-comment/new-comment.component';
-import { NewCommentButtonComponent } from './new-comment-button/new-comment-button.component';
-import { NewCommentTextComponent } from './new-comment-text/new-comment-text.component';
+import { ShowAuthorComponent } from './show-author/show-author.component';
 import { ShowCommentComponent } from './show-comment/show-comment.component';
-import { ShowCommentAuthorComponent } from './show-comment-author/show-comment-author.component';
-import { ShowCommentTextComponent } from './show-comment-text/show-comment-text.component';
 import { ShowCommentsComponent } from './show-comments/show-comments.component';
+import { ShowTargetComponent } from './show-target/show-target.component';
+
+const allComponents = [
+  CreateAuthorComponent, CreateCommentComponent, CreateTargetComponent,
+  EditCommentComponent, ShowAuthorComponent, ShowCommentComponent,
+  ShowCommentsComponent, ShowTargetComponent
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DvModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    // Material
+    MatButtonModule, MatDatepickerModule, MatInputModule, MatSelectModule,
+    MatFormFieldModule,
+    MatMomentDateModule
   ],
-  declarations: [EditCommentComponent, NewCommentComponent, NewCommentButtonComponent, NewCommentTextComponent, ShowCommentComponent, ShowCommentAuthorComponent, ShowCommentTextComponent, ShowCommentsComponent]
+  declarations: allComponents,
+  exports: allComponents,
+  entryComponents: allComponents
 })
 export class CommentModule { }
