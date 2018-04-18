d31ee4f1544dc0fdc18be28a14507029a36a10b4
Santiago P De Rosso <spderosso@gmail.com>
2018-04-18 00:36:35 -0400

    tx processing

--- packages/dv-gateway/src/gateway.ts
+++ packages/dv-gateway/src/gateway.ts
@@ -6,23 +6,102 @@ import * as path from 'path';
 import * as _ from 'lodash';
 import * as request from 'superagent';
 
+import { TxCoordinator, TxConfig, Vote } from './txCoordinator';
+
+
+interface Config {
+  dbHost: string;
+  dbPort: number;
+  wsPort: number;
+  dbName: string;
+  reinitDbOnStartup: boolean;
+}
+
+interface ActionInfo {
+  childActions: {[childActionName: string]: ActionInfo};
+}
+
+interface RootAction {
+  rootName: string;
+  childActions: {[childActionName: string]: ActionInfo};
+}
 
 interface DvConfig {
   name: string;
   startServer?: boolean;
   watch?: boolean;
   config?: any;
-  gatewayPort?: number;
+  gateway: { config: Config };
   usedCliches?: {[as: string]: DvConfig};
+  actionTree?: RootAction;
 }
 
-// Get DvConfig
-const argv = minimist(process.argv);
-const configFilePath = argv.configFilePath;
-const dvConfig: DvConfig = JSON.parse(readFileSync(configFilePath, 'utf8'));
-console.log(`Using dv config ${JSON.stringify(dvConfig, undefined, 2)}`);
+const DEFAULT_CONFIG: Config = {
+  dbHost: 'localhost',
+  dbPort: 27017,
+  wsPort: 3000,
+  dbName: `gateway-db`,
+  reinitDbOnStartup: true
+};
+
+
+function getDvConfig(): DvConfig {
+  const argv = minimist(process.argv);
+  const configFilePath = argv.configFilePath;
+  const dvConfig: DvConfig = JSON.parse(readFileSync(configFilePath, 'utf8'));
+  console.log(`Using dv config ${JSON.stringify(dvConfig, undefined, 2)}`);
+  return dvConfig;
+}
+
+const dvConfig: DvConfig = getDvConfig();
+const config: Config = {...DEFAULT_CONFIG, ...dvConfig.gateway.config};
+
+interface VotePayload {
+  status: number;
+  text: string;
+}
+
+const txConfig: TxConfig<
+  GatewayToClicheRequest, VotePayload, express.Response> = {
+  dbHost: config.dbHost,
+  dbPort: config.dbPort,
+  dbName: config.dbName,
+  reinitDbOnStartup: config.reinitDbOnStartup,
+  sendCommitToCohort: (gcr: GatewayToClicheRequest): Promise<void> => {
+    gcr.path = '/commit' + gcr.path;
+    return forwardRequest(gcr).then(unusedResp => undefined);
+  },
+  sendAbortToCohort: (gcr: GatewayToClicheRequest): Promise<void> => {
+    gcr.path = '/abort' +  gcr.path;
+    return forwardRequest(gcr).then(unusedResp => undefined);
+  },
+  sendVoteToCohort: (gcr: GatewayToClicheRequest): Promise<Vote<VotePayload>> => {
+    gcr.path = '/vote' + gcr.path;
+    return forwardRequest<Vote<string>>(gcr)
+      .then((resp: {status: number; text: Vote<string>}): Vote<VotePayload> => {
+        return {
+          result: resp.text.result,
+          payload: { status: resp.status, text: resp.text.payload }
+        };
+      });
+  },
+  sendAbortToClient: (gcr: GatewayToClicheRequest, res?: express.Response) => {
+    res!.status(500);
+    res!.send('the tx this action is part of aborted');
+  },
+  sendToClient: (payload: VotePayload, res?: express.Response) =>  {
+    res!.status(payload.status);
+    res!.send(payload.text);
+  },
+  getCohorts: (cohortId: string) => {
+    // would also need the actionConfig
+    return ['foo', 'bar'];
+  }
+};
 
 const app = express();
+const txCoordinator = new TxCoordinator<
+  GatewayToClicheRequest, VotePayload, express.Response>(txConfig);
 
 // Handle API requests
 // projects contains all keys in `usedCliches` plus the name of the current app
@@ -49,16 +128,26 @@ interface RequestOptions {
 
 interface GatewayRequest {
   from: string[];
-  path?: string;
+  runId?: string | undefined;
+  path?: string | undefined;
   options?: RequestOptions;
 }
 
-app.use('/api', bodyParser.json(), (req, res, next) => {
+interface GatewayToClicheRequest extends GatewayRequest {
+  url: string;
+  method: string;
+  body: string;
+}
+
+
+app.use('/api', bodyParser.json(), async (req, res, next) => {
   const gatewayRequest: GatewayRequest = {
     from: JSON.parse(req.query.from),
+    runId: req.query.runId,
     path: req.query.path,
     options: req.query.options ? JSON.parse(req.query.options) : undefined
   };
+  // Validate request
   if (!req.query.from) {
     res.status(500).send('No from specified');
     return;
@@ -73,12 +162,51 @@ app.use('/api', bodyParser.json(), (req, res, next) => {
       JSON.stringify(dstTable, undefined, 2));
     return;
   }
-  console.log(`to:${to}, port: ${dstTable[to]}`);
-  let url = `http://localhost:${dstTable[to]}`;
-  if (gatewayRequest.path) {
-    url += gatewayRequest.path;
+  const actionPath = getActionPath(gatewayRequest.from, projects);
+  if (!actionPathIsValid(actionPath, dvConfig.actionTree)) {
+    res.status(500).send(
+      `Invalid action path: ${actionPath}, my actionConfig is ` +
+      JSON.stringify(dvConfig.actionTree, undefined, 2));
+    return;
   }
-  let clicheReq = request(req.method, url);
+
+
+  const runId = gatewayRequest.runId;
+  console.log(
+    'Processing request:' + `to: ${to}, port: ${dstTable[to]}, ` +
+    `action path: ${actionPath}, runId: ${runId}` +
+    (isDvTx(actionPath) ? `, dvTxId: ${runId}` : 'not part of a tx'));
+
+  const gatewayToClicheRequest = {
+    ...gatewayRequest,
+    ...{
+     url: `http://localhost:${dstTable[to]}`,
+     method: req.method,
+     body: req.body
+    }
+  };
+  if (!isDvTx(actionPath)) {
+    const clicheRes: {status: number, text: string} = await forwardRequest<string>(gatewayToClicheRequest);
+    res.status(clicheRes.status);
+    res.send(clicheRes.text);
+  } else {
+    if (!runId) {
+      throw new Error('run id undefined');
+    }
+    await txCoordinator
+      .processMessage(runId, actionPath.join('-'), gatewayToClicheRequest, res);
+  }
+});
+
+/**
+ *  Forwards to the cliche the given request.
+ **/
+async function forwardRequest<T>(gatewayRequest: GatewayToClicheRequest)
+  : Promise<{status: number, text: T}> {
+  if (gatewayRequest.path) {
+    gatewayRequest.url += gatewayRequest.path;
+  }
+  let clicheReq = request(gatewayRequest.method, gatewayRequest.url);
   if (gatewayRequest.options) {
     if (gatewayRequest.options.params) {
       clicheReq = clicheReq.query(gatewayRequest.options.params);
@@ -87,19 +215,11 @@ app.use('/api', bodyParser.json(), (req, res, next) => {
       clicheReq = clicheReq.set(gatewayRequest.options.headers);
     }
   }
-  clicheReq.send(req.body);
-  clicheReq.end((clicheErr, clicheRes) => {
-    if (clicheErr) {
-      console.log(`Got back ${JSON.stringify(clicheErr)}`);
-      const send = clicheErr.text ? clicheErr.text : clicheErr.response.text;
-      res.status(clicheErr.status).send(send);
-    } else {
-      console.log(`Got back ${JSON.stringify(clicheRes)}`);
-      const send = clicheRes.text ? clicheRes.text : clicheErr.response.text;
-      res.status(clicheRes.status).send(send);
-    }
-  });
-});
+  clicheReq.send(gatewayRequest.body);
+  const response: request.Response = await clicheReq.end();
+  console.log(`Got back ${JSON.stringify(response)}`);
+  return {status: response.status, text: JSON.parse(response.text)};
+}
 
 // Serve SPA
 const distFolder = path.join(process.cwd(), 'dist');
@@ -109,7 +229,7 @@ app.get('*', ({}, res) => {
 });
 
 // Listen
-const port = dvConfig.gatewayPort;
+const port = config.wsPort;
 app.listen(port, () => {
   console.log(`Running gateway on port ${port}`);
   console.log(`Using config ${JSON.stringify(dvConfig, undefined, 2)}`);
@@ -117,7 +237,62 @@ app.listen(port, () => {
 });
 
 
-// Utility functions
+
+
+/**
+ *  Checks that the given `actionPath` corresponds to a valid path according
+ *  to `actionConfig`.
+ */
+function actionPathIsValid(
+  actionPath: string[], actionTree: (RootAction | undefined)): boolean {
+  // For now, if the action tree is undefined we don't do any validation
+  if (!actionTree) {
+    return true;
+  } else if (_.isEmpty(actionPath)) {
+    return false;
+  } else if (actionPath[0] !== actionTree.rootName) {
+    return false;
+  } else if (actionPath.length === 1) {
+    return true;
+  }
+
+  return _actionPathIsValid(actionPath.slice(1), actionTree);
+}
+
+function _actionPathIsValid(actionPath: string[], action: ActionInfo): boolean {
+  if (_.isEmpty(action.childActions)) {
+    return false;
+  }
+  const next = actionPath[0];
+  if (actionPath.length === 1) {
+    return next in action.childActions;
+  }
+
+  if (!(next in action.childActions)) {
+    return false;
+  }
+  return _actionPathIsValid(actionPath.slice(1), action.childActions[next]);
+}
+
+// returns an array [action_1, action_2, ..., action_n] representing an action
+// path from action_1 to action_n where action_n is the action that originated
+// the request.
+// Note: dv-* actions are included
+// In other words, it filters non-dv nodes from `from`
+function getActionPath(from: string[], projects: Set<string>): string[]{
+  return _.chain(from)
+    .map((node) => node.toLowerCase())
+    .filter((name) => {
+      const project = name.split('-')[0];
+      return projects.has(project) || project === 'dv';
+    })
+    .reverse()
+    .value();
+}
+
+function isDvTx(actionPath: string[]) {
+  return _.includes(actionPath, 'dv-tx');
+}
 
 // from: originatingAction-action-....-action
 function getDst(thisProject: string, from: string[], projects: Set<string>)
---
+++ packages/dv-gateway/src/txCoordinator.ts
@@ -0,0 +1,329 @@
+import { MongoClient, Collection } from 'mongodb';
+import * as AsyncLock from 'async-lock';
+import * as EventEmitter from 'events';
+
+import * as _ from 'lodash';
+
+
+// Note that messages and payloads are persisted for logging purposes and
+// to recover a tx if the coordinator or one of the cohorts fails permanently.
+// To pass transient information that should be associated with a message use
+// the optional `state: State` value.
+export interface TxConfig<Message, Payload, State = any> {
+  dbHost: string;
+  dbPort: number;
+  dbName: string;
+  reinitDbOnStartup: boolean;
+  // msg is the original message processed
+  // While the return value of these functions is ignored they should only
+  // resolve to a value if the cohort responded to the commit/abort (only an
+  // ACK is needed)
+  sendCommitToCohort: (msg: Message) => Promise<void>;
+  sendAbortToCohort: (msg: Message) => Promise<void>;
+  sendVoteToCohort: (msg: Message) => Promise<Vote<Payload>>;
+  sendAbortToClient: (msg: Message, state?: State) => void;
+  // payload is what got returned in `sendVoteToCohort`
+  sendToClient: (payload: Payload, state?: State) => void;
+  getCohorts: (cohortId: string) => string[];
+}
+
+export interface Vote<Payload> {
+  result: 'yes' | 'no';
+  payload: Payload;
+}
+
+type TxStatus = (
+  'voting' | 'committing' | 'committed' | 'aborting' | 'aborted');
+
+// cohorts can be in an additional state in which they voted and are now waiting
+// for a resolution
+type CohortStatus = TxStatus | 'waitingForCompletion';
+
+interface Cohort<Message, Payload> {
+  msg: Message;
+  vote: Vote<Payload> | undefined;
+  status: CohortStatus;
+}
+
+type CohortMap<Message, Payload> = {
+  [cohortId: string]: Cohort<Message, Payload> | undefined
+};
+interface TxDoc<Message, Payload> {
+  id: string;
+  status: TxStatus;
+  // Could be undefined if we haven't processed message from the cohort yet
+  cohorts: CohortMap<Message, Payload>;
+  // When the tx was started
+  startedOn: Date;
+}
+
+interface Transition {
+  newTxStatus?: 'committing' | 'aborting';
+  newCohortStatus?: 'waitingForCompletion';
+}
+
+// The time in seconds after which a transaction stuck in voting should abort
+const TX_TIMEOUT_SECONDS = 3;
+
+/**
+ *  Coordinates between a set of participants on whether to commit or abort
+ *  a transaction.
+ **/
+export class TxCoordinator<Message, Payload, State = any> {
+  // The tx id are used as the keys for locking
+  private lock = new AsyncLock();
+  // Each tx emits a txId-[commit | abort] event
+  private completed = new EventEmitter();
+  private txs: Collection<TxDoc<Message, Payload>>;
+
+  constructor(private config: TxConfig<Message, Payload, State>) {
+    this.getTxsCollection().then(txs => {
+      this.txs = txs;
+    });
+    setInterval(this.timeoutAbort, TX_TIMEOUT_SECONDS);
+  }
+
+  private async getTx(txId: string, cohortId: string)
+    : Promise<TxDoc<Message, Payload>> {
+    // Look at the tx table and create one if there's no active tx for txId
+    return (await this.txs.findOneAndUpdate(
+      { id: txId },
+      { $setOnInsert: {
+        status: 'voting',
+        $currentDate: { startedOn: true },
+        // We are going to be recomputing the expected cohorts for each tx we
+        // fetch but this lets us do `$setOnInsert`. We could check if the tx
+        // has been initialized and if not compute the expected cohorts but we
+        // would have to acquire the tx lock to do `find`, get the expected
+        // cohorts, and `update` atomically.
+        cohorts: _.zipObject(this.config.getCohorts(cohortId))
+      } },
+      { returnOriginal: false, upsert: true }))
+      .value!;
+  }
+
+  async processMessage(
+    txId: string, cohortId: string, msg: Message, state?: State)
+    : Promise<void> {
+    const tx: TxDoc<Message, Payload> = await this.getTx(txId, cohortId);
+
+    // no race condition here because the set of cohorts doesn't change after
+    // initialization
+    if (!_.includes(_.keys(tx.cohorts), cohortId)) {
+      // We received a request from a cohort that is not part of the tx
+      throw new Error(`[txId: ${txId}] ${cohortId} is not part of this tx`);
+    }
+
+    // If we got here the tx has been initialized (by this msg or a previous one)
+    // The tx status could be 'voting', 'aborting' or 'aborted'
+
+    // We might still end up sending an unnecessary vote message if the tx
+    // changes to abort right after we do the check but that won't cause any
+    // problems. The check here is mostly to save some unnecessary votes.
+    if (this.shouldAbort(tx)) {
+      this.config.sendAbortToClient(msg, state);
+      return;
+    }
+
+    // tx is 'voting'
+
+    // Here we are using mongodb to essentially lock on the txId and cohortId
+    // to detect duplicate requests and if o/w send the vote (so that we never
+    // send a duplicate vote to a cohort)
+    // If `cohorts.${cohortId}` is not undefined then this is a duplicate req
+    const update = await this.txs.updateOne(
+      { id: txId, [`cohorts.${cohortId}`]: undefined },
+      { $set: {
+        [`cohorts.${cohortId}`]: {
+          msg: msg, vote: undefined, status: 'voting'
+        }
+      } });
+    if (update.matchedCount == 0) { // Duplicate request
+      throw new Error(`[txId: ${txId}] Duplicate message from ${cohortId}`);
+    }
+    const vote: Vote<Payload> = await this.config.sendVoteToCohort(msg);
+    this.saveVote(txId, cohortId, vote);
+
+    // We need to be sure that we are processing votes one by one because:
+    // (i) as soon as one votes 'no' we are going to abort, and
+    // (ii) when we receive the last 'yes' we are going to commit.
+    const transition: Transition = await this.lock.acquire(txId, () => {
+      return this.processVote(txId, cohortId, vote, () => {
+          this.config.sendToClient(vote.payload);
+      }, () => {
+          this.config.sendAbortToClient(msg, state);
+      });
+    });
+
+    let ret;
+    if (_.isEmpty(transition)) {  // tx was already aborting
+      this.config.sendAbortToClient(msg, state);
+      this.completed.emit(txId + '-abort');
+      ret = this.completeMessage(txId, cohortId, msg, false);
+    } else if (transition.newTxStatus === 'committing' &&
+               !transition.newCohortStatus) {
+      this.config.sendToClient(vote.payload);
+      this.completed.emit(txId + '-commit');
+      ret = Promise.all([
+        this.completeTx(txId, true),
+        this.completeMessage(txId, cohortId, msg, true)]);
+    } else if (!transition.newTxStatus &&
+               transition.newCohortStatus === 'waitingForCompletion') {
+      // nothing to do in this case since we are waiting
+    } else if (transition.newTxStatus === 'aborting') {
+      this.config.sendAbortToClient(msg);
+      this.completed.emit(txId + '-abort');
+      ret = Promise.all([
+        this.completeTx(txId, false),
+        this.completeMessage(txId, cohortId, msg, false)]);
+    } else {
+      throw new Error('Invalid transition');
+    }
+    return ret;
+  }
+
+  private async processVote(
+    txId: string, cohortId: string, vote: Vote<Payload>,
+    onCommit: () => void, onAbort: () => void): Promise<Transition> {
+    // Txs are never removed and if we got here we know there's a tx doc
+    const tx: TxDoc<Message, Payload> = (await this.txs.findOne({ id: txId }))!;
+    const ret: Transition = {};
+
+    if (this.shouldAbort(tx)) {
+      return ret;
+    }
+    // We are still in the 'voting' phase. The tx can't be committing
+    // because it doesn't yet have this vote
+
+    if (vote.result === 'yes') {
+      if (this.allVotedButOne(tx)) {
+        // We know that all previous votes were 'yes' because if o/w the
+        // state would be 'aborting'/'aborted'
+        await this.updateTxStatus(txId, 'committing');
+        ret.newTxStatus = 'committing';
+      } else { // we are not the last one
+        // wait on complete
+        this.completed.on(txId + '-commit', onCommit);
+        this.completed.on(txId + '-abort', onAbort);
+        await this.updateCohortStatus(txId, cohortId, 'waitingForCompletion');
+        ret.newCohortStatus = 'waitingForCompletion';
+      }
+    } else { // the vote was 'no'
+      // We know that all previous votes were 'yes' because if o/w the state
+      // would be 'aborting'/'aborted'. Thus, we are in the 'voting' phase and
+      // this is the first 'no' vote
+      await Promise.all([
+        this.updateCohortStatus(txId, cohortId, 'waitingForCompletion'),
+        this.updateTxStatus(txId, 'aborting')]);
+      ret.newTxStatus = 'aborting';
+      ret.newCohortStatus = 'waitingForCompletion';
+    }
+    return ret;
+  }
+
+  private shouldAbort(tx: TxDoc<Message, Payload>) {
+    return tx.status === 'aborting' || tx.status === 'aborted';
+  }
+
+  private allVotedButOne(tx: TxDoc<Message, Payload>): boolean {
+    return !_.includes(tx.cohorts, undefined) &&
+      _.chain(tx.cohorts)
+        .map('status')
+        .filter((s) => s === 'voting')
+        .value()
+        .length === 1;
+  }
+
+  private saveVote(txId: string, cohortId: string, vote: Vote<Payload>) {
+    return this.txs
+      .updateOne(
+        { id: txId },
+        { $set: { [`cohorts.${cohortId}.vote`]: vote } });
+  }
+
+  private updateCohortStatus(
+    txId: string, cohortId: string, newStatus: CohortStatus) {
+    return this.txs
+      .updateOne(
+        { id: txId},
+        { $set: { [`cohorts.${cohortId}.status`]: newStatus } });
+  }
+
+  private updateTxStatus(id: string, newStatus: CohortStatus) {
+    return this.txs.updateOne({ id: id }, { $set: { state: newStatus } });
+  }
+
+  /**
+   *  Every time this function runs, it aborts active txs whose
+   *  `startedOn + TX_TIMEOUT_SECONDS` time is less than the current time.
+   **/
+  private async timeoutAbort() {
+    const threshold = new Date();
+    threshold.setSeconds(threshold.getSeconds() - TX_TIMEOUT_SECONDS);
+    const txsToAbort: {id: string}[] = await this.txs.find<{id: string}>({
+      status: 'voting',
+      startedOn: { $gte: threshold }
+    }, { projection: { id: 1 } }).toArray();
+
+    await Promise.all(
+      _.map(txsToAbort, async (txToAbort: TxDoc<Message, Payload>) => {
+        return this.lock.acquire(txToAbort.id, async () => {
+            const tx: TxDoc<Message, Payload> = (await this.txs
+              .findOne({ id: txToAbort.id }, { projection: { status: 1 }}))!;
+            if (tx.status !== 'voting') {
+              return false;
+            }
+            await this.updateTxStatus(txToAbort.id, 'aborting');
+            return true;
+          })
+          .then((shouldCompleteTx: boolean): Promise<any> | undefined => {
+            if (shouldCompleteTx) {
+              return this.completeTx(txToAbort.id, false);
+            }
+            return;
+          });
+      })
+    );
+  }
+
+  private completeMessage(
+    txId: string, cohortId: string, msg: Message, success: boolean) {
+    const completedStatus = success ? 'committed' : 'aborted';
+    const completingStatus = success ? 'committing' : 'aborting';
+    const send = success ? this.config.sendCommitToCohort :
+      this.config.sendAbortToCohort;
+    return this.updateCohortStatus(txId, cohortId, completingStatus)
+      // TODO: catch errors
+      .then(unused => send(msg))
+      .then(unused => this.updateCohortStatus(txId, cohortId, completedStatus));
+  }
+
+  private async completeTx(txId: string, success: boolean) {
+    const completedStatus = success ? 'committed' : 'aborted';
+    const tx: { cohorts: CohortMap<Message, Payload> }= (await this.txs
+      .findOne({ id: txId }, { projection: { 'cohorts': 1 }}))!;
+    await Promise.all(
+      _.map(
+        tx.cohorts, (cohort: Cohort<Message, Payload>, cohortId: string) => {
+        if (cohort.status !== 'waitingForCompletion') {
+          return;
+        }
+        return this.completeMessage(txId, cohortId, cohort.msg, success);
+      }));
+    return this.updateTxStatus(txId, completedStatus);
+  }
+
+  private async getTxsCollection() {
+    const client = await MongoClient.connect(
+      `mongodb://${this.config.dbHost}:${this.config.dbPort}`);
+    const db = client.db(this.config.dbName);
+    if (this.config.reinitDbOnStartup) {
+      await db.dropDatabase();
+      console.log(`Reinitialized db ${this.config.dbName}`);
+    }
+    const txs = db.collection('txs');
+    txs.createIndex({ id: 1 }, { unique: true });
+    txs.createIndex({ 'cohorts.id': 1 }, { unique: true });
+    return txs;
+  }
+}
