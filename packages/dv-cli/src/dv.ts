fb1b20fc9b55ae1fecf0a6f5e2681a1cf49da937
Maryam Archie <marchie@mit.edu>
2018-04-21 15:25:54 -0400

    add shell param for windows usage

--- packages/dv-cli/src/dv.ts
+++ packages/dv-cli/src/dv.ts
@@ -17,7 +17,7 @@ export function npm(args: string[], cwd?: string): void {
 }
 
 function cmd(cmd: string, args: string[], cwd?: string): void {
-  const c = spawnSync(cmd, args, { stdio: 'inherit', cwd: cwd });
+  const c = spawnSync(cmd, args, { stdio: 'inherit', cwd: cwd, shell: true });
   if (c.error) {
     throw new Error(`Failed to run "${cmd}": ${c.error}`);
   }
