3cbdd7504e3a5039664afdbc02654bbab43a6f44
Santiago P De Rosso <spderosso@gmail.com>
2018-10-04 16:11:50 -0400

    add some inputs to show-item-count

--- packages/catalog/transfer/src/app/transfer/show-item-count/show-item-count.component.ts
+++ packages/catalog/transfer/src/app/transfer/show-item-count/show-item-count.component.ts
@@ -2,10 +2,14 @@ import { Component, Input } from '@angular/core';
 import { ItemCount } from '../transfer.config';
 
 @Component({
-  selector: 'transfer-show-item',
+  selector: 'transfer-show-item-count',
   templateUrl: './show-item-count.component.html',
   styleUrls: ['./show-item-count.component.css']
 })
 export class ShowItemCountComponent {
   @Input() itemCount: ItemCount;
+
+  // Presentation inputs
+  @Input() showItemId = true;
+  @Input() showCount = true;
 }
