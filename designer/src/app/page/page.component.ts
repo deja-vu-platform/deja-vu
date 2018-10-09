698fb812b58454cfe680ebfd313c14d78191071a
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-10-09 19:59:36 -0400

    Created Widget component for dynamic loading

--- designer/src/app/page/page.component.ts
+++ designer/src/app/page/page.component.ts
@@ -1,37 +1,13 @@
-import { Component, AfterViewInit, ViewChildren, QueryList, ComponentFactoryResolver, ChangeDetectorRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { ɵe as CreateWeeklySeriesComponent } from 'event'; // TODO: proper import
-import { WidgetDirective } from '../widget.directive';
-
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
-export class PageComponent implements AfterViewInit {
+export class PageComponent {
   widgets = [CreateWeeklySeriesComponent];
-  @ViewChildren(WidgetDirective) widgetHosts: QueryList<WidgetDirective>;
 
-
-  constructor(
-    private componentFactoryResolver: ComponentFactoryResolver,
-    private cdr: ChangeDetectorRef
-  ) { }
-
-  ngAfterViewInit() {
-    this.loadWidgets();
-  }
-
-  loadWidgets() {
-    this.widgetHosts.forEach((widgetHost, i) => {
-      const widget = this.widgets[i];
-      const componentFactory = this.componentFactoryResolver.resolveComponentFactory(widget);
-      console.log(this.widgetHosts);
-      const viewContainerRef = widgetHost.viewContainerRef;
-      viewContainerRef.clear();
-      viewContainerRef.createComponent(componentFactory);
-    });
-    this.cdr.detectChanges();
-  }
-
+  constructor() { }
 }
---
+++ designer/src/app/widget/widget.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, ViewChild, ComponentFactoryResolver } from '@angular/core';
+import { WidgetDirective } from '../widget.directive';
+
+@Component({
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.scss']
+})
+export class WidgetComponent implements OnInit {
+  @Input() widget;
+  @ViewChild(WidgetDirective) widgetHost: WidgetDirective;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  ngOnInit() {
+    this.loadWidget();
+  }
+
+  loadWidget() {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.widget);
+    const viewContainerRef = this.widgetHost.viewContainerRef;
+    viewContainerRef.clear();
+    viewContainerRef.createComponent(componentFactory);
+  }
+}
