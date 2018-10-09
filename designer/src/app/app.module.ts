0f41098c61c0bfdfb1950f4f30e00d44194502dc
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-10-09 14:51:33 -0400

    Started widget list, better styling

--- designer/src/app/app.module.ts
+++ designer/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { EventModule } from 'event';
 import { GATEWAY_URL, DvModule } from 'dv-core';
+
+import { MatModule } from './mat/mat.module';
 
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './main-view/main-view.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { ClicheListComponent } from './cliche-list/cliche-list.component';
+import { WidgetListComponent } from './widget-list/widget-list.component';
 
 
 @NgModule({
@@ -16,9 +21,13 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     MainViewComponent,
     SideMenuComponent,
     TopBarComponent,
+    ClicheListComponent,
+    WidgetListComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatModule,
     DvModule,
     EventModule,
   ],
