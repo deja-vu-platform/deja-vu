56ca9b7b44e36885e4bb7d43e3ac5e6f1498fa5e
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-10-09 16:27:52 -0400

    Load a widget dynamically.

---
+++ designer/src/app/widget.directive.spec.ts
@@ -0,0 +1,8 @@
+import { WidgetDirective } from './widget.directive';
+
+describe('WidgetDirective', () => {
+  it('should create an instance', () => {
+    const directive = new WidgetDirective(null); // TODO: viewContainerRef argument
+    expect(directive).toBeTruthy();
+  });
+});
