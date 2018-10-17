f2feb49deb27e1b536e3d2881ae98acac1b46e1a
Czarina Lao <mcslao@mit.edu>
2018-10-17 16:35:48 -0400

    Migrate comment cliche to use cliche server package

---
+++ packages/catalog/comment/server/schema.ts
@@ -0,0 +1,35 @@
+export interface CommentDoc {
+  id: string;
+  authorId: string;
+  targetId: string;
+  content: string;
+  pending?: PendingDoc;
+}
+
+export interface PendingDoc {
+  reqId: string;
+  type: 'create-comment' | 'edit-comment';
+}
+
+export interface CreateCommentInput {
+  id?: string;
+  authorId: string;
+  targetId: string;
+  content: string;
+}
+
+export interface EditCommentInput {
+  id: string;
+  authorId: string;
+  content: string;
+}
+
+export interface CommentInput {
+  byAuthorId: string;
+  ofTargetId: string;
+}
+
+export interface CommentsInput {
+  byAuthorId?: string;
+  ofTargetId?: string;
+}
