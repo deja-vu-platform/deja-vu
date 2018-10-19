f089416b150dfd317badd094a116ae6aa57d5213
Czarina Lao <mcslao@mit.edu>
2018-10-18 23:38:04 -0400

    Migrate property cliche to use cliche server package

--- packages/catalog/property/server/server.ts
+++ packages/catalog/property/server/server.ts
@@ -1,89 +1,14 @@
-import * as bodyParser from 'body-parser';
-import * as express from 'express';
-import { readFileSync } from 'fs';
-import * as minimist from 'minimist';
-import * as mongodb from 'mongodb';
-import * as path from 'path';
-import { v4 as uuid } from 'uuid';
-
-// GitHub Issue: https://github.com/apollographql/apollo-server/issues/927
-// tslint:disable-next-line:no-var-requires
-const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
-import { makeExecutableSchema } from 'graphql-tools';
-
-import * as _ from 'lodash';
-
 import * as Ajv from 'ajv';
-
-
-interface Config {
-  wsPort: number;
-  dbHost: string;
-  dbPort: number;
-  dbName: string;
-  reinitDbOnStartup: boolean;
-  initialObjects: Object[];
-  schema: any;
-}
-
-interface Pending {
-  _pending?: PendingDoc;
-}
-
-interface ObjectDoc extends Pending {
-  [field: string]: any;
-}
-
-interface PendingDoc {
-  reqId: string;
-}
-
-const argv = minimist(process.argv);
-
-const name = argv.as ? argv.as : 'property';
-
-const DEFAULT_CONFIG: Config = {
-  dbHost: 'localhost',
-  dbPort: 27017,
-  wsPort: 3000,
-  dbName: `${name}-db`,
-  reinitDbOnStartup: true,
-  initialObjects: [],
-  schema: {}
-};
-
-let configArg;
-try {
-  configArg = JSON.parse(argv.config);
-} catch (e) {
-  throw new Error(`Couldn't parse config ${argv.config}`);
-}
-
-const config: Config = {...DEFAULT_CONFIG, ...configArg};
-
-console.log(`Connecting to mongo server ${config.dbHost}:${config.dbPort}`);
-
-let db: mongodb.Db, objects: mongodb.Collection<ObjectDoc>;
-mongodb.MongoClient.connect(
-  `mongodb://${config.dbHost}:${config.dbPort}`, async (err, client) => {
-    if (err) {
-      throw err;
-    }
-    db = client.db(config.dbName);
-    if (config.reinitDbOnStartup) {
-      await db.dropDatabase();
-      console.log(`Reinitialized db ${config.dbName}`);
-    }
-    objects = db.collection('objects');
-    objects.createIndex({ id: 1 }, { unique: true, sparse: true });
-    if (!_.isEmpty(config.initialObjects)) {
-      objects.insertMany(_.map(config.initialObjects, (obj) => {
-        obj.id = obj.id ? obj.id : uuid();
-
-        return obj;
-      }));
-    }
-  });
+import {
+  ClicheServer,
+  ClicheServerBuilder,
+  Config,
+  Context
+} from 'cliche-server';
+import * as _ from 'lodash';
+import * as mongodb from 'mongodb';
+import { ObjectDoc } from './schema';
+import { v4 as uuid } from 'uuid';
 
 const jsonSchemaTypeToGraphQlType = {
   integer: 'Int',
@@ -92,37 +17,53 @@ const jsonSchemaTypeToGraphQlType = {
   boolean: 'Boolean'
   /* Not supported yet: object, array, null */
 };
-const requiredProperties: Set<string> = new Set(config.schema.required);
-const properties = _
-  .chain(config.schema.properties)
-  .toPairs()
-  .map(([propertyName, schemaPropertyObject]) => {
-    return `${propertyName}: ` +
-      jsonSchemaTypeToGraphQlType[schemaPropertyObject.type] +
-      (requiredProperties.has(propertyName) ? '!' : '');
-  })
-  .value();
 
-const joinedProperties = properties.join('\n');
+interface Property {
+  title: { type: keyof typeof jsonSchemaTypeToGraphQlType }
+}
 
-const dynamicTypeDefs = [`
-  type Object {
-    id: ID!
-    ${joinedProperties}
-  }
+interface Schema {
+  properties: Property[];
+  required?: string[];
+  title: string;
+  type: 'object'
+}
 
-  input CreateObjectInput {
-    id: ID
-    ${joinedProperties}
-  }
-`];
+interface PropertyConfig extends Config {
+  initialObjects: Object[];
+  schema: Schema;
+}
 
-const typeDefs = [
-  readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
-  ...dynamicTypeDefs
-];
+function getDynamicTypeDefs(uncastConfig: Config): string[] {
+  const config: PropertyConfig = uncastConfig as PropertyConfig;
+  const requiredProperties: Set<string> = new Set(config.schema.required);
+  const properties = _
+    .chain(config.schema.properties)
+    .toPairs()
+    .map(([propertyName, schemaPropertyObject]) => {
+      return `${propertyName}: ` +
+        jsonSchemaTypeToGraphQlType[schemaPropertyObject.type] +
+        (requiredProperties.has(propertyName) ? '!' : '');
+    })
+    .value();
 
-function createObjectFromInput(input) {
+  const joinedProperties = properties.join('\n');
+
+  return [`
+    type Object {
+      id: ID!
+      ${joinedProperties}
+    }
+
+    input CreateObjectInput {
+      id: ID
+      ${joinedProperties}
+    }
+  `];
+}
+
+function createObjectFromInput(uncastConfig: Config, input) {
+  const config: PropertyConfig = uncastConfig as PropertyConfig;
   const newObject = input;
   newObject.id = input.id ? input.id : uuid();
   const ajv = new Ajv();
@@ -135,145 +76,122 @@ function createObjectFromInput(input) {
   return newObject;
 }
 
-interface Context {
-  reqType: 'vote' | 'commit' | 'abort' | undefined;
-  runId: string;
-  reqId: string;
-}
+function resolvers(db: mongodb.Db, uncastConfig: Config): object {
+  const config: PropertyConfig = uncastConfig as PropertyConfig;
+  const objects: mongodb.Collection<ObjectDoc> = db.collection('objects');
+  const resolversObj = {
+    Query: {
+      property: (root, { name }) => {
+        const propertyInfo = config.schema.properties[name];
 
-const resolvers = {
-  Query: {
-    property: (root, { name }) => {
-      const propertyInfo = config.schema.properties[name];
+        return {
+          name: name,
+          schema: JSON.stringify(propertyInfo),
+          required: _.includes(config.schema.required, name)
+        };
+      },
+      object: async (root, { id }) => {
+        const obj: ObjectDoc | null = await objects.findOne({ id: id });
 
-      return {
-        name: name,
-        schema: JSON.stringify(propertyInfo),
-        required: _.includes(config.schema.required, name)
-      };
+        return _.get(obj, '_pending') ? null : obj;
+      },
+      objects: (root) => objects.find({ _pending: { $exists: false } })
+        .toArray(),
+      properties: (root) => _
+        .chain(config['schema'].properties)
+        .toPairs()
+        .map(([ name, propertyInfo ]) => ({
+          name: name,
+          schema: JSON.stringify(propertyInfo),
+          required: _.includes(config.schema.required, name)
+        }))
+        .value()
     },
-    object: async (root, { id }) => {
-      const obj: ObjectDoc | null = await objects.findOne({ id: id });
-
-      return _.get(obj, '_pending') ? null : obj;
+    Property: {
+      name: (root) => root.name,
+      schema: (root) => root.schema,
+      required: (root) => root.required
     },
-    objects: (root) => objects.find({ _pending: { $exists: false } })
-      .toArray(),
-    properties: (root) => _
-      .chain(config.schema.properties)
-      .toPairs()
-      .map(([ name, propertyInfo ]) => ({
-        name: name,
-        schema: JSON.stringify(propertyInfo),
-        required: _.includes(config.schema.required, name)
-      }))
-      .value()
-  },
-  Property: {
-    name: (root) => root.name,
-    schema: (root) => root.schema,
-    required: (root) => root.required
-  },
-  Mutation: {
-    createObject: async (root, { input }, context: Context) => {
-      const newObject: ObjectDoc = createObjectFromInput(input);
-      const reqIdPendingFilter = { '_pending.reqId': context.reqId };
-      switch (context.reqType) {
-        case 'vote':
-          newObject._pending = { reqId: context.reqId };
-          /* falls through */
-        case undefined:
-          await objects.insertOne(newObject);
+    Mutation: {
+      createObject: async (root, { input }, context: Context) => {
+        const newObject: ObjectDoc = createObjectFromInput(config, input);
+        const reqIdPendingFilter = { '_pending.reqId': context.reqId };
+        switch (context.reqType) {
+          case 'vote':
+            newObject._pending = { reqId: context.reqId };
+            /* falls through */
+          case undefined:
+            await objects.insertOne(newObject);
 
-          return newObject;
-        case 'commit':
-          await objects.updateOne(
-            reqIdPendingFilter, { $unset: { _pending: '' } });
+            return newObject;
+          case 'commit':
+            await objects.updateOne(
+              reqIdPendingFilter, { $unset: { _pending: '' } });
 
-          return;
-        case 'abort':
-          await objects.deleteOne(reqIdPendingFilter);
+            return;
+          case 'abort':
+            await objects.deleteOne(reqIdPendingFilter);
 
-          return;
-      }
-    },
+            return;
+        }
 
-    createObjects: async (root, { input }, context: Context) => {
-      const objDocs: ObjectDoc[] = _.map(input, createObjectFromInput);
-      const reqIdPendingFilter = { '_pending.reqId': context.reqId };
-      switch (context.reqType) {
-        case 'vote':
-          _.each(objDocs, (objDoc: ObjectDoc) => {
-            objDoc._pending = { reqId: context.reqId };
-          });
-          /* falls through */
-        case undefined:
-          await objects.insertMany(objDocs);
+        return;
+      },
 
-          return objDocs;
-        case 'commit':
-          await objects.updateMany(
-            reqIdPendingFilter, { $unset: { _pending: '' } });
+      createObjects: async (root, { input }, context: Context) => {
+        const objDocs: ObjectDoc[] = _.map(input, createObjectFromInput);
+        const reqIdPendingFilter = { '_pending.reqId': context.reqId };
+        switch (context.reqType) {
+          case 'vote':
+            _.each(objDocs, (objDoc: ObjectDoc) => {
+              objDoc._pending = { reqId: context.reqId };
+            });
+            /* falls through */
+          case undefined:
+            await objects.insertMany(objDocs);
 
-          return;
-        case 'abort':
-          await objects.deleteMany(reqIdPendingFilter);
+            return objDocs;
+          case 'commit':
+            await objects.updateMany(
+              reqIdPendingFilter, { $unset: { _pending: '' } });
 
-          return;
+            return;
+          case 'abort':
+            await objects.deleteMany(reqIdPendingFilter);
+
+            return;
+        }
+
+        return;
       }
     }
+  };
+  const objectResolvers = {
+    id: (obj) => obj.id
+  };
+  for (const propertyName of _.keys(config.schema.properties)) {
+    objectResolvers[propertyName] = (obj) => obj[propertyName];
   }
+  resolversObj['Object'] = objectResolvers;
+  return resolversObj;
 };
 
-const objectResolvers = {
-  id: (obj) => obj.id
-};
-for (const propertyName of _.keys(config.schema.properties)) {
-  objectResolvers[propertyName] = (obj) => obj[propertyName];
-}
-resolvers['Object'] = objectResolvers;
+const propertyCliche: ClicheServer = new ClicheServerBuilder('property')
+  .initDb(async (db: mongodb.Db, uncastConfig: Config): Promise<any> => {
+    const config: PropertyConfig = uncastConfig as PropertyConfig;
+    const objects: mongodb.Collection<ObjectDoc> = db.collection('objects');
+    await objects.createIndex({ id: 1 }, { unique: true, sparse: true });
+    if (!_.isEmpty(config.initialObjects)) {
+      return objects.insertMany(_.map(config.initialObjects, (obj) => {
+        obj.id = obj.id ? obj.id : uuid();
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-const app = express();
-
-app.post(/^\/dv\/(.*)\/(vote|commit|abort)\/.*/,
-  (req, res, next) => {
-    req['reqId'] = req.params[0];
-    req['reqType'] = req.params[1];
-    next();
-  },
-  bodyParser.json(),
-  graphqlExpress((req) => {
-    return {
-      schema: schema,
-      context: {
-        reqType: req!['reqType'],
-        reqId: req!['reqId']
-      },
-      formatResponse: (gqlResp) => {
-        const reqType = req!['reqType'];
-        switch (reqType) {
-          case 'vote':
-            return {
-              result: (gqlResp.errors) ? 'no' : 'yes',
-              payload: gqlResp
-            };
-          case 'abort':
-          case 'commit':
-            return 'ACK';
-          case undefined:
-            return gqlResp;
-        }
-      }
-    };
+        return obj;
+      }));
+    }
+    return Promise.resolve();
   })
-);
+  .resolvers(resolvers)
+  .dynamicTypeDefs(getDynamicTypeDefs)
+  .build();
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-app.listen(config.wsPort, () => {
-  console.log(`Running ${name} with config ${JSON.stringify(config)}`);
-});
+propertyCliche.start();
