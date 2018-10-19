33597ed7752ab1c87fd652995330cb47e539465e
Czarina Lao <mcslao@mit.edu>
2018-10-18 23:56:25 -0400

    Migrate passkey cliche to use cliche server package

--- packages/catalog/passkey/server/server.ts
+++ packages/catalog/passkey/server/server.ts
@@ -1,19 +1,25 @@
-import * as bodyParser from 'body-parser';
-import * as express from 'express';
-import { readFileSync } from 'fs';
+import {
+  ClicheServer,
+  ClicheServerBuilder,
+  CONCURRENT_UPDATE_ERROR,
+  Config,
+  Context,
+  Validation
+} from 'cliche-server';
 import * as jwt from 'jsonwebtoken';
-import * as minimist from 'minimist';
+import * as _ from 'lodash';
 import * as mongodb from 'mongodb';
 import * as path from 'path';
+import {
+  PasskeyDoc,
+  PendingDoc,
+  SignInOutput,
+  VerifyInput,
+} from './schema';
 import * as shajs from 'sha.js';
+import { v4 as uuid } from 'uuid';
 import { WORDS } from './words';
 
-// GitHub Issue: https://github.com/apollographql/apollo-server/issues/927
-// tslint:disable-next-line:no-var-requires
-const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
-import { makeExecutableSchema } from 'graphql-tools';
-
-import * as _ from 'lodash';
 
 // TODO: Update passkey.validate.ts if any changes made
 const PASSKEY_MIN_LENGTH = 3;
@@ -24,81 +30,9 @@ const WORDS_SIZE = WORDS.length;
 // TODO: Change before deployment
 const SECRET_KEY = 'ultra-secret-key';
 
-interface PasskeyDoc {
-  code: string;
-  pending?: PendingDoc;
-}
-
-interface PendingDoc {
-  reqId: string;
-  type: 'create-passkey';
-}
-
-interface VerifyInput {
-  code: string;
-  token: string;
-}
-
-interface SignInOutput {
-  token: string;
-  passkey: PasskeyDoc;
-}
-
-interface Config {
-  wsPort: number;
-  dbHost: string;
-  dbPort: number;
-  dbName: string;
-  reinitDbOnStartup: boolean;
-}
-
-const CONCURRENT_UPDATE_ERROR = 'An error has occured. Please try again later';
-
-const argv = minimist(process.argv);
-
-const name = argv.as ? argv.as : 'passkey';
-
-const DEFAULT_CONFIG: Config = {
-  dbHost: 'localhost',
-  dbPort: 27017,
-  wsPort: 3000,
-  dbName: `${name}-db`,
-  reinitDbOnStartup: true
-};
-
-let configArg;
-try {
-  configArg = JSON.parse(argv.config);
-} catch (e) {
-  throw new Error(`Couldn't parse config ${argv.config}`);
-}
-
-const config: Config = { ...DEFAULT_CONFIG, ...configArg };
-
-console.log(`Connecting to mongo server ${config.dbHost}:${config.dbPort}`);
-let db: mongodb.Db;
-let passkeys: mongodb.Collection<PasskeyDoc>;
-mongodb.MongoClient.connect(
-  `mongodb://${config.dbHost}:${config.dbPort}`, async (err, client) => {
-    if (err) {
-      throw err;
-    }
-    db = client.db(config.dbName);
-    if (config.reinitDbOnStartup) {
-      await db.dropDatabase();
-      console.log(`Reinitialized db ${config.dbName}`);
-    }
-
-    passkeys = db.collection('passkeys');
-    passkeys.createIndex({ code: 1 }, { unique: true, sparse: true });
-  });
-
-
-const typeDefs = [readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8')];
-
-class Validation {
-
-  static async passkeyExistsOrFail(code: string): Promise<PasskeyDoc> {
+class PasskeyValidation {
+  static async passkeyExistsOrFail(passkeys: mongodb.Collection<PasskeyDoc>,
+    code: string): Promise<PasskeyDoc> {
     const hashedCode = getHashedCode(code);
     const passkey: PasskeyDoc | null = await passkeys
       .findOne({ code: hashedCode });
@@ -117,12 +51,6 @@ class Validation {
       ${PASSKEY_MIN_LENGTH}-${PASSKEY_MAX_LENGTH} characters long.`);
     }
   }
-}
-
-interface Context {
-  reqType: 'vote' | 'commit' | 'abort' | undefined;
-  runId: string;
-  reqId: string;
 }
 
 function isPendingCreate(passkey: PasskeyDoc | null) {
@@ -152,7 +80,7 @@ function verify(token: string, code: string): boolean {
  * Generates a random code.
  * @returns{string} A unique 5-7 letter english word not found in the database.
  */
-async function getRandomPasscode() {
+async function getRandomPasscode(passkeys: mongodb.Collection<PasskeyDoc>) {
   const randomIndex = Math.floor(Math.random() * WORDS_SIZE);
   const code = WORDS[randomIndex];
 
@@ -161,21 +89,22 @@ async function getRandomPasscode() {
     .then((passkey) => {
       if (!passkey) { return code; }
 
-      return getRandomPasscode();
+      return getRandomPasscode(passkeys);
     });
 }
 
-async function createPasskey(code: string, context: Context) {
+async function createPasskey(passkeys: mongodb.Collection<PasskeyDoc>,
+  code: string, context: Context) {
   if (_.isEmpty(code)) {
     // Generate random code
-    code = await getRandomPasscode();
+    code = await getRandomPasscode(passkeys);
   } else {
     const passkey = await passkeys.findOne({ code: getHashedCode(code) });
     if (passkey) {
       throw new Error(`Passkey ${code} already exists`);
     }
 
-    Validation.passkeyIsValidOrFail(code);
+    PasskeyValidation.passkeyIsValidOrFail(code);
   }
 
   const newPasskey: PasskeyDoc = { code: getHashedCode(code) };
@@ -202,108 +131,76 @@ async function createPasskey(code: string, context: Context) {
 
 }
 
-const resolvers = {
-  Query: {
-    passkeys: () => passkeys.find({ pending: { $exists: false } })
-      .toArray(),
+function resolvers(db: mongodb.Db, config: Config): object {
+  const passkeys: mongodb.Collection<PasskeyDoc> = db.collection('passkeys');
+  return {
+    Query: {
+      passkeys: () => passkeys.find({ pending: { $exists: false } })
+        .toArray(),
 
-    passkey: async (root, { code }) => {
-      const passkey: PasskeyDoc = await Validation.passkeyExistsOrFail(code);
+      passkey: async (root, { code }) => {
+        const passkey: PasskeyDoc = await PasskeyValidation.passkeyExistsOrFail(
+          passkeys, code);
 
-      return isPendingCreate(passkey) ? null : passkey;
+        return isPendingCreate(passkey) ? null : passkey;
+      },
+
+      verify: (root, { input }: { input: VerifyInput }) =>
+        verify(input.token, input.code)
     },
 
-    verify: (root, { input }: { input: VerifyInput }) =>
-      verify(input.token, input.code)
-  },
-
-  Passkey: {
-    code: (passkey: PasskeyDoc) => passkey.code
-  },
-
-  SignInOutput: {
-    token: (signInOutput: SignInOutput) => signInOutput.token,
-    passkey: (signInOutput: SignInOutput) => signInOutput.passkey
-  },
-
-  Mutation: {
-    createPasskey: async (root, { code }, context: Context) => {
-      const passkey = await createPasskey(code, context);
-
-      return passkey;
+    Passkey: {
+      code: (passkey: PasskeyDoc) => passkey.code
     },
 
-    createAndValidatePasskey: async (root, { code }, context: Context) => {
-      const passkey = await createPasskey(code, context);
+    SignInOutput: {
+      token: (signInOutput: SignInOutput) => signInOutput.token,
+      passkey: (signInOutput: SignInOutput) => signInOutput.passkey
+    },
 
-      if (!_.isNil(passkey)) {
-        const token: string = sign(passkey!.code);
+    Mutation: {
+      createPasskey: async (root, { code }, context: Context) => {
+        const passkey = await createPasskey(passkeys, code, context);
+
+        return passkey;
+      },
+
+      createAndValidatePasskey: async (root, { code }, context: Context) => {
+        const passkey = await createPasskey(passkeys, code, context);
+
+        if (!_.isNil(passkey)) {
+          const token: string = sign(passkey!.code);
+
+          return {
+            token: token,
+            passkey: passkey
+          };
+        }
+
+        return;
+      },
+
+      validatePasskey: async (root, { code }) => {
+        const passkey = await PasskeyValidation.passkeyExistsOrFail(
+          passkeys, code);
+
+        const token: string = sign(code);
 
         return {
           token: token,
           passkey: passkey
         };
       }
-
-      return;
-    },
-
-    validatePasskey: async (root, { code }) => {
-      const passkey = await Validation.passkeyExistsOrFail(code);
-
-      const token: string = sign(code);
-
-      return {
-        token: token,
-        passkey: passkey
-      };
     }
-  }
+  };
 };
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-const app = express();
-
-app.post(/^\/dv\/(.*)\/(vote|commit|abort)\/.*/,
-  (req, res, next) => {
-    req['reqId'] = req.params[0];
-    req['reqType'] = req.params[1];
-    next();
-  },
-  bodyParser.json(),
-  graphqlExpress((req) => {
-    return {
-      schema: schema,
-      context: {
-        reqType: req!['reqType'],
-        reqId: req!['reqId']
-      },
-      formatResponse: (gqlResp) => {
-        const reqType = req!['reqType'];
-        switch (reqType) {
-          case 'vote':
-            return {
-              result: (gqlResp.errors) ? 'no' : 'yes',
-              payload: gqlResp
-            };
-          case 'abort':
-          case 'commit':
-            return 'ACK';
-          case undefined:
-            return gqlResp;
-        }
-      }
-    };
+const passkeyCliche: ClicheServer = new ClicheServerBuilder('passkey')
+  .initDb((db: mongodb.Db, config: Config): Promise<any> => {
+    const passkeys: mongodb.Collection<PasskeyDoc> = db.collection('passkeys');
+    return passkeys.createIndex({ id: 1 }, { unique: true, sparse: true });
   })
-);
+  .resolvers(resolvers)
+  .build();
 
-app.use('/graphql', bodyParser.json(), bodyParser.urlencoded({
-  extended: true
-}), graphqlExpress({ schema }));
-
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-app.listen(config.wsPort, () => {
-  console.log(`Running ${name} with config ${JSON.stringify(config)}`);
-});
+passkeyCliche.start();
