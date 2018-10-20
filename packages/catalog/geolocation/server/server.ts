ece80f96e7017c72dc1e9362adc79801eb7fe0c9
Czarina Lao <mcslao@mit.edu>
2018-10-20 04:47:00 -0400

    Fix version of @types/mongodb on more cliches and a few typos and type errors

--- packages/catalog/geolocation/server/server.ts
+++ packages/catalog/geolocation/server/server.ts
@@ -40,131 +40,132 @@ function isPendingCreate(doc: MarkerDoc | null) {
 function resolvers(db: mongodb.Db, config: Config): object {
   const markers: mongodb.Collection<MarkerDoc> = db.collection('markers');
   return {
-  Query: {
-    marker: async (root, { id }) => {
-      const marker = await MarkerValidation.markerExistsOrFail(markers, id);
-      if (_.isNil(marker) || isPendingCreate(marker)) {
-        throw new Error(`Marker ${id} does not exist`);
-      }
+    Query: {
+      marker: async (root, { id }) => {
+        const marker = await MarkerValidation.markerExistsOrFail(markers, id);
+        if (_.isNil(marker) || isPendingCreate(marker)) {
+          throw new Error(`Marker ${id} does not exist`);
+        }
 
-      return markerDocToMarker(marker);
+        return markerDocToMarker(marker);
+      },
+
+      markers: (root, { input }: { input: MarkersInput }) => {
+        const filter = { pending: { $exists: false } };
+        if (input.ofMapId) {
+          // Get markers by map
+          filter['mapId'] = input.ofMapId;
+
+          return markers.find(filter)
+            .toArray();
+        } else {
+          // Get all markers
+          return markers.find(filter)
+            .toArray();
+        }
+      }
     },
 
-    markers: (root, { input }: { input: MarkersInput }) => {
-      const filter = { pending: { $exists: false } };
-      if (input.ofMapId) {
-        // Get markers by map
-        filter['mapId'] = input.ofMapId;
-
-        return markers.find(filter)
-          .toArray();
-      } else {
-        // Get all markers
-        return markers.find(filter)
-          .toArray();
-      }
-    }
-  },
-
-  Marker: {
-    id: (marker: MarkerDoc) => marker.id,
-    title: (marker: MarkerDoc) => marker.title,
-    latitude: (marker: MarkerDoc) => marker.location.coordinates[1],
-    longitude: (marker: MarkerDoc) => marker.location.coordinates[0],
-    mapId: (marker: MarkerDoc) => marker.mapId
-  },
-
-  Mutation: {
-    createMarker: async (
-      root, { input }: { input: CreateMarkerInput }, context: Context) => {
-      const newMarker: MarkerDoc = {
-        id: input.id ? input.id : uuid(),
-        title: input.title ? input.title : '',
-        location: {
-          type: 'Point',
-          coordinates: [input.longitude, input.latitude]
-        },
-        mapId: input.mapId
-      };
-
-      const reqIdPendingFilter = { 'pending.reqId': context.reqId };
-
-      switch (context.reqType) {
-        case 'vote':
-          newMarker.pending = {
-            reqId: context.reqId,
-            type: 'create-marker'
-          };
-        /* falls through */
-        case undefined:
-          await markers.insertOne(newMarker);
-
-          return newMarker;
-        case 'commit':
-          await markers.updateOne(
-            reqIdPendingFilter,
-            { $unset: { pending: '' } });
-
-          return;
-        case 'abort':
-          await markers.deleteOne(reqIdPendingFilter);
-
-          return;
-      }
-
-      return newMarker;
+    Marker: {
+      id: (marker: MarkerDoc) => marker.id,
+      title: (marker: MarkerDoc) => marker.title,
+      latitude: (marker: MarkerDoc) => marker.location.coordinates[1],
+      longitude: (marker: MarkerDoc) => marker.location.coordinates[0],
+      mapId: (marker: MarkerDoc) => marker.mapId
     },
 
-    deleteMarker: async (root, { id }, context: Context) => {
-      const notPendingResourceFilter = {
-        id: id,
-        pending: { $exists: false }
-      };
-      const reqIdPendingFilter = { 'pending.reqId': context.reqId };
+    Mutation: {
+      createMarker: async (
+        root, { input }: { input: CreateMarkerInput }, context: Context) => {
+        const newMarker: MarkerDoc = {
+          id: input.id ? input.id : uuid(),
+          title: input.title ? input.title : '',
+          location: {
+            type: 'Point',
+            coordinates: [input.longitude, input.latitude]
+          },
+          mapId: input.mapId
+        };
 
-      switch (context.reqType) {
-        case 'vote':
-          await MarkerValidation.markerExistsOrFail(markers, id);
-          const pendingUpdateObj = await markers.updateOne(
-            notPendingResourceFilter,
-            {
-              $set: {
-                pending: {
-                  reqId: context.reqId,
-                  type: 'delete-marker'
+        const reqIdPendingFilter = { 'pending.reqId': context.reqId };
+
+        switch (context.reqType) {
+          case 'vote':
+            newMarker.pending = {
+              reqId: context.reqId,
+              type: 'create-marker'
+            };
+          /* falls through */
+          case undefined:
+            await markers.insertOne(newMarker);
+
+            return newMarker;
+          case 'commit':
+            await markers.updateOne(
+              reqIdPendingFilter,
+              { $unset: { pending: '' } });
+
+            return;
+          case 'abort':
+            await markers.deleteOne(reqIdPendingFilter);
+
+            return;
+        }
+
+        return newMarker;
+      },
+
+      deleteMarker: async (root, { id }, context: Context) => {
+        const notPendingResourceFilter = {
+          id: id,
+          pending: { $exists: false }
+        };
+        const reqIdPendingFilter = { 'pending.reqId': context.reqId };
+
+        switch (context.reqType) {
+          case 'vote':
+            await MarkerValidation.markerExistsOrFail(markers, id);
+            const pendingUpdateObj = await markers.updateOne(
+              notPendingResourceFilter,
+              {
+                $set: {
+                  pending: {
+                    reqId: context.reqId,
+                    type: 'delete-marker'
+                  }
                 }
-              }
-            });
+              });
 
-          if (pendingUpdateObj.matchedCount === 0) {
-            throw new Error(CONCURRENT_UPDATE_ERROR);
-          }
+            if (pendingUpdateObj.matchedCount === 0) {
+              throw new Error(CONCURRENT_UPDATE_ERROR);
+            }
 
-          return true;
-        case undefined:
-          await MarkerValidation.markerExistsOrFail(markers, id);
-          const res = await markers
-            .deleteOne({ id: id, pending: { $exists: false } });
+            return true;
+          case undefined:
+            await MarkerValidation.markerExistsOrFail(markers, id);
+            const res = await markers
+              .deleteOne({ id: id, pending: { $exists: false } });
 
-          if (res.deletedCount === 0) {
-            throw new Error(CONCURRENT_UPDATE_ERROR);
-          }
+            if (res.deletedCount === 0) {
+              throw new Error(CONCURRENT_UPDATE_ERROR);
+            }
 
-          return true;
-        case 'commit':
-          await markers.deleteOne(reqIdPendingFilter);
+            return true;
+          case 'commit':
+            await markers.deleteOne(reqIdPendingFilter);
 
-          return;
-        case 'abort':
-          await markers.updateOne(
-            reqIdPendingFilter, { $unset: { pending: '' } });
+            return;
+          case 'abort':
+            await markers.updateOne(
+              reqIdPendingFilter, { $unset: { pending: '' } });
 
-          return;
+            return;
+        }
+
+        return;
       }
-
-      return;
     }
-  }
+  };
 };
 
 const geolocationCliche: ClicheServer = new ClicheServerBuilder('geolocation')
