365875f3295205bd1f2a8b96f8af0fbbb6a9f30a
Czarina Lao <mcslao@mit.edu>
2018-10-25 13:23:19 -0400

    Revert temp fix for ng-packagr problem

---
+++ packages/dv-core/public_api.ts
@@ -0,0 +1,4 @@
+export * from './src/app/dv/gateway.service';
+export * from './src/app/dv/run.service';
+export { FieldMap, ValueMap, Action }  from './src/app/dv/include/include.component';
+export * from './src/app/dv/dv.module';
--- packages/dv-core/src/public_api.ts
+++
@@ -1,4 +0,0 @@
-export * from './app/dv/gateway.service';
-export * from './app/dv/run.service';
-export { FieldMap, ValueMap, Action }  from './app/dv/include/include.component';
-export * from './app/dv/dv.module';
