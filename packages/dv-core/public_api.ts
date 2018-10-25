195d6a8d20f10a735ac8855d36be18207fe46b45
Czarina Lao <mcslao@mit.edu>
2018-10-24 22:59:05 -0400

    Move public_api to fix ng-packagr problem See https://github.com/ng-packagr/ng-packagr/issues/589

--- packages/dv-core/public_api.ts
+++
@@ -1,4 +0,0 @@
-export * from './src/app/dv/gateway.service';
-export * from './src/app/dv/run.service';
-export { FieldMap, ValueMap, Action }  from './src/app/dv/include/include.component';
-export * from './src/app/dv/dv.module';
---
+++ packages/dv-core/src/public_api.ts
@@ -0,0 +1,4 @@
+export * from './app/dv/gateway.service';
+export * from './app/dv/run.service';
+export { FieldMap, ValueMap, Action }  from './app/dv/include/include.component';
+export * from './app/dv/dv.module';
