74cd6dd5d7981c8177c5bb28a2f32abe0673fcb2
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-10-29 18:17:42 -0400

    Remove market. Rewards are now properties.

--- samples/chorestar/src/app/app.module.ts
+++ samples/chorestar/src/app/app.module.ts
@@ -9,9 +9,9 @@ import { AppComponent } from './app.component';
 import { AuthenticationModule } from 'authentication';
 import { AuthorizationModule } from 'authorization';
 import { GroupModule } from 'group';
-import { MarketModule } from 'market';
 import { PropertyModule } from 'property';
 import { TaskModule } from 'task';
+import { TRANSFER_CONFIG, TransferModule } from 'transfer';
 
 import { ChildHomeComponent } from './child-home/child-home.component';
 import { LandingComponent } from './landing/landing.component';
@@ -41,14 +41,15 @@ import { ShowRewardComponent } from './show-reward/show-reward.component';
     ]),
     AuthenticationModule,
     AuthorizationModule,
-    MarketModule,
+    TransferModule,
     TaskModule,
     GroupModule,
     PropertyModule
   ],
-  providers: [{
-    provide: GATEWAY_URL, useValue: 'http://localhost:3000/api'
-  }],
+  providers: [
+    { provide: GATEWAY_URL, useValue: 'http://localhost:3000/api'},
+    { provide: TRANSFER_CONFIG, useValue: { balanceType: 'money' } }
+  ],
   bootstrap: [AppComponent],
   entryComponents: [
     ShowChoreComponent,
