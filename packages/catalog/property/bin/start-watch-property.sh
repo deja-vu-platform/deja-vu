add457cf720c028d7200a84927de8776de3bf601
Maryam Archie <marchie@mit.edu>
2018-10-29 01:59:22 -0400

    Update property cliche to use relaxed-json

--- packages/catalog/property/bin/start-watch-property.sh
+++ packages/catalog/property/bin/start-watch-property.sh
@@ -1,1 +1,1 @@
-if [ -f dist/server/server.js ] then nodemon -w dist/server dist/server/server.js -- --config "`dv get config`" else echo 'No file' fi
+if [ -f dist/server/server.js ] then nodemon -w dist/server dist/server/server.js -- --config `dv get config` else echo 'No file' fi
