0680bb700fa8b4ee771b57238997df86dc224d37
Czarina Lao <mcslao@mit.edu>
2018-11-05 22:39:30 -0500

    Add scoring for comments and fix some html/css

--- samples/hackernews/src/app/show-comment/show-comment.component.ts
+++ samples/hackernews/src/app/show-comment/show-comment.component.ts
@@ -8,6 +8,10 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ShowCommentComponent implements OnInit {
   showComment = ShowCommentComponent;
   @Input() comment: any;
+  // TODO: collapsing functionality
+  // collapsed: boolean = false;
+  // @Input() collapsible: boolean;
+  @Input() showAncestorSummary: boolean = false;
 
   constructor() {
   }
