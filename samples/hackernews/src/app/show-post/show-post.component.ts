5662fcd4230a468984807bb9a8eefb2b78d58653
Czarina Lao <mcslao@mit.edu>
2018-11-06 00:04:40 -0500

    Get and show base url of post

--- samples/hackernews/src/app/show-post/show-post.component.ts
+++ samples/hackernews/src/app/show-post/show-post.component.ts
@@ -18,4 +18,17 @@ export class ShowPostComponent implements OnInit {
   outputPost(value) {
     this.loadedPost.emit(value);
   }
+
+  getBaseUrl(url: string): string {
+    // regex from https://stackoverflow.com/a/17773849
+    const urlMatcher = new RegExp([
+      /https?:\/\/(www\.)?/,                          // https://www.
+      /([-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6})/,  // base url, group 2
+      /\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)?/].map(r => r.source).join(''), 'i');
+
+    const baseUrlGroupIndex = 2;
+    const groups = url.match(urlMatcher);
+    return (groups && groups.length > baseUrlGroupIndex) ?
+      groups[baseUrlGroupIndex] : '';
+  }
 }
