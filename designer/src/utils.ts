1b08b1e9a0cf14b71dce88e94bcb2ca192132b52
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-11-15 12:46:49 -0500

    Remove unused filterInPlace

--- designer/src/utils.ts
+++ designer/src/utils.ts
@@ -1,15 +1,3 @@
-export function filterInPlace<T>(arr: T[], f: (e: T, i: number) => boolean) {
-  let out = 0;
-  for (let i = 0; i < arr.length; i += 1) {
-    if (f(arr[i], i)) {
-      arr[out] = arr[i];
-      out += 1;
-    }
-  }
-  arr.length = out;
-  return arr;
-}
-
 export function isString(variable) {
   return (typeof variable === 'string' || variable instanceof String);
 }
