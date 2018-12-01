fc8eb8caedd3c965c52b96e25868868190fe220b
Santiago P De Rosso <spderosso@gmail.com>
2018-12-01 17:59:59 -0500

    add dv-if to exports

--- packages/dv-core/src/app/dv/dv.module.ts
+++ packages/dv-core/src/app/dv/dv.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule
@@ -7,29 +7,30 @@ import {
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
-import { TxComponent } from './tx/tx.component';
+import { ButtonLastComponent } from './button-last/button-last.component';
+import { ButtonComponent } from './button/button.component';
+import { CallbackLinkComponent } from './callback-link/callback-link.component';
+import { CallbackComponent } from './callback/callback.component';
+import { ChooseComponent } from './choose/choose.component';
+import { GatewayServiceFactory } from './gateway.service';
 import { IdComponent } from './id/id.component';
 import { IdsComponent } from './ids/ids.component';
-import { InputIdComponent } from './input-id/input-id.component';
-import { StageComponent } from './stage/stage.component';
-import { ButtonComponent } from './button/button.component';
-import { ButtonLastComponent } from './button-last/button-last.component';
-import { LinkComponent } from './link/link.component';
-import { StatusComponent } from './status/status.component';
-import { MergeComponent } from './merge/merge.component';
-import { ChooseComponent } from './choose/choose.component';
-import { CallbackComponent } from './callback/callback.component';
-import { CallbackLinkComponent } from './callback-link/callback-link.component';
-import { RedirectComponent } from './redirect/redirect.component';
+import { IfComponent } from './if/if.component';
 import { IncludeComponent, IncludeDirective } from './include/include.component';
-import { GatewayServiceFactory } from './gateway.service';
+import { InputIdComponent } from './input-id/input-id.component';
+import { LinkComponent } from './link/link.component';
+import { MergeComponent } from './merge/merge.component';
+import { RedirectComponent } from './redirect/redirect.component';
 import { RunService } from './run.service';
+import { StageComponent } from './stage/stage.component';
+import { StatusComponent } from './status/status.component';
+import { TxComponent } from './tx/tx.component';
 
 import { OfDirective } from './of.directive';
 
 
 const allComponents = [
-  IdComponent, IdsComponent, TxComponent, IncludeComponent,
+  IdComponent, IdsComponent, TxComponent, IncludeComponent, IfComponent,
   IncludeDirective, ButtonLastComponent, ButtonComponent, LinkComponent,
   StatusComponent, MergeComponent, InputIdComponent, StageComponent,
   ChooseComponent, CallbackComponent, CallbackLinkComponent,
