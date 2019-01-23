4a82298c8581db13179b567a0400071e065f3969
Czarina Lao <mcslao@mit.edu>
2019-01-23 12:19:21 +0800

    Change scoring to ranking

--- packages/catalog/ranking/e2e/app.e2e-spec.ts
+++ packages/catalog/ranking/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('scoring App', () => {
+describe('ranking App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('scoring App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to scoring!');
+    expect(page.getParagraphText()).toEqual('Welcome to ranking!');
   });
 });
--- packages/catalog/ranking/e2e/app.po.ts
+++ packages/catalog/ranking/e2e/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('scoring-root h1')).getText();
+    return element(by.css('ranking-root h1')).getText();
   }
 }
