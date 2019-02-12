cac5dd6f51203b27ca407b6581bc2ec134f5b219
Czarina Lao <mcslao@mit.edu>
2019-02-12 17:11:38 -0500

    Add documentation for zip component

--- packages/core/src/app/dv/zip/zip.component.ts
+++ packages/core/src/app/dv/zip/zip.component.ts
@@ -5,6 +5,17 @@ import {
 import * as _ from 'lodash';
 
 
+/**
+ * Zips arrays of input values into objects with the corresponding field names.
+ * Requires: inputs.length === fieldNames.length
+ * and inputs[i].length === inputs[j].length === n for every pair i, j
+ * so that the emitted objects.length === n
+ * 
+ * For example, if inputs=[[1,2], [a,b], [x,y]]
+ * and fieldNames=['id', 'letter1', 'letter2'],
+ * The objects emitted will be
+ *   [{ id: 1, letter1: a, letter2: x }, { id: 2, letter1: b, letter2: y }]
+ */
 @Component({
   selector: 'dv-zip',
   templateUrl: './zip.component.html'
