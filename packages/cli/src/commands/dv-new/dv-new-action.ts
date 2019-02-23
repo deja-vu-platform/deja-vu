187e0fa902e6f4058df5ffd89a4173dcc7e9c66b
Czarina Lao <mcslao@mit.edu>
2019-02-23 00:22:59 -0500

    Allow user to skip metadata and app.component.html additions from the cli

--- packages/cli/src/commands/dv-new/dv-new-action.ts
+++ packages/cli/src/commands/dv-new/dv-new-action.ts
@@ -8,7 +8,19 @@ import {
 
 exports.command = 'action <name>';
 exports.desc = 'create a new action';
-exports.handler = ({ name }) => {
+exports.builder = {
+  skipAppComponentHtml: {
+    default: false,
+    describe: 'When set, the new action will not be added to the app.component.html file.',
+    type: 'boolean'
+  },
+  skipMetadataImport: {
+    default: false,
+    describe: 'When set, the new action will not be added to the metadata file.',
+    type: 'boolean'
+  }
+};
+exports.handler = ({ name, skipAppComponentHtml, skipMetadataImport }) => {
   if (!isInNgProjectRoot()) {
     console.log('Please run this command from the root of a cliché directory.');
     return;
@@ -18,6 +30,15 @@ exports.handler = ({ name }) => {
 
   const clicheName = projectName();
   const schematicsPkgName = getDvPackageName('schematics');
-  ng(['generate', `${schematicsPkgName}:action`, `--clicheName=${clicheName}`,
-    `--actionName=${name}`]);
+
+  ng(['generate',
+    `${schematicsPkgName}:action`,
+    `--clicheName=${clicheName}`,
+    `--actionName=${name}`,
+    getOptionalFlag(skipAppComponentHtml, '--skipAppComponentHtml'),
+    getOptionalFlag(skipMetadataImport, '--skipMetadataImport')]);
 };
+
+function getOptionalFlag(flag: boolean, flagName: string) {
+  return flag ? `--${flagName}` : '';
+}
