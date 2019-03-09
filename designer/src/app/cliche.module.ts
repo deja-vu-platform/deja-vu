b29ff53dcca0d1a19c36bdcbbdfdcc4273393a37
Barry Anthony McNamara III <bamazap@hotmail.com>
2019-03-08 22:00:33 -0500

    Fix floating menu padding.

--- designer/src/app/cliche.module.ts
+++ designer/src/app/cliche.module.ts
@@ -160,7 +160,7 @@ export const dvCoreActions = dvCliche.actions
 dvCliche.actions.push(({
   name: 'text',
   component: <Component>TextComponent,
-  inputs: ['*html'],
+  inputs: [],
   outputs: [],
   actionInputs: {}
 }));
