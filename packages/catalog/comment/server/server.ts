08b31dedcabca8783f8367d38953e4e9eeef9ffa
Czarina Lao <mcslao@mit.edu>
2019-03-13 16:24:53 -0400

    Mostly update comment to use the new db layer, leaving a few unsupported validation

--- packages/catalog/comment/server/server.ts
+++ packages/catalog/comment/server/server.ts
@@ -1,15 +1,15 @@
 import {
   ActionRequestTable,
+  ClicheDb,
   ClicheServer,
   ClicheServerBuilder,
-  CONCURRENT_UPDATE_ERROR,
+  Collection,
   Config,
   Context,
   getReturnFields,
   Validation
 } from '@deja-vu/cliche-server';
 import * as _ from 'lodash';
-import * as mongodb from 'mongodb';
 import {
   CommentDoc,
   CommentInput,
@@ -24,7 +24,7 @@ import { v4 as uuid } from 'uuid';
 
 class CommentValidation {
   static async commentExistsOrFails(
-    comments: mongodb.Collection<CommentDoc>, id: string): Promise<CommentDoc> {
+    comments: Collection<CommentDoc>, id: string): Promise<CommentDoc> {
     return Validation.existsOrFail(comments, id, 'Comment');
   }
 }
@@ -83,41 +83,20 @@ const actionRequestTable: ActionRequestTable = {
   `
 };
 
-function isPendingCreate(doc: CommentDoc | null) {
-  return _.get(doc, 'pending.type') === 'create-comment';
-}
-
-function resolvers(db: mongodb.Db, _config: Config): object {
-  const comments: mongodb.Collection<CommentDoc> = db.collection('comments');
+function resolvers(db: ClicheDb, _config: Config): object {
+  const comments: Collection<CommentDoc> = db.collection('comments');
 
   return {
     Query: {
-      comment: async (_root, { id }) => {
-        const comment = await CommentValidation.commentExistsOrFails(
-          comments, id);
-
-        if (_.isNil(comment) || isPendingCreate(comment)) {
-          throw new Error(`Comment ${id} not found`);
-        }
-
-        return comment;
-      },
+      comment: async (_root, { id }) => await comments.findOne({ id }),
 
       commentByAuthorTarget: async (
-        _root, { input }: { input: CommentInput }) => {
-        const comment = await comments.findOne({
+        _root, { input }: { input: CommentInput }) => await comments.findOne({
           authorId: input.byAuthorId, targetId: input.ofTargetId
-        });
-
-        if (_.isNil(comment) || isPendingCreate(comment)) {
-          throw new Error(`Comment not found`);
-        }
-
-        return comment;
-      },
+        }),
 
       comments: async (_root, { input }: { input: CommentsInput }) => {
-        const filter = { pending: { $exists: false } };
+        const filter = {};
         if (!_.isEmpty(input.byAuthorId)) {
           // Comments by an author
           filter['authorId'] = input.byAuthorId;
@@ -127,8 +106,7 @@ function resolvers(db: mongodb.Db, _config: Config): object {
           filter['targetId'] = input.ofTargetId;
         }
 
-        return comments.find(filter)
-          .toArray();
+        return comments.find(filter);
       }
     },
 
@@ -148,158 +126,48 @@ function resolvers(db: mongodb.Db, _config: Config): object {
           targetId: input.targetId,
           content: input.content
         };
-        const reqIdPendingFilter = { 'pending.reqId': context.reqId };
 
-        switch (context.reqType) {
-          case 'vote':
-            newComment.pending = {
-              reqId: context.reqId,
-              type: 'create-comment'
-            };
-          /* falls through */
-          case undefined:
-            await comments.insertOne(newComment);
-
-            return newComment;
-          case 'commit':
-            await comments.updateOne(
-              reqIdPendingFilter,
-              { $unset: { pending: '' } });
-
-            return undefined;
-          case 'abort':
-            await comments.deleteOne(reqIdPendingFilter);
-
-            return undefined;
-        }
-
-        return newComment;
+        return await comments.insertOne(context, newComment);
       },
 
       editComment: async (
         _root, { input }: { input: EditCommentInput }, context: Context) => {
         const comment = await CommentValidation.commentExistsOrFails(
           comments, input.id);
-
+        // TODO: make this atomic/is this check even needed?
+        // users could use authorization for this
+        // so that the functionality to edit other people's comments
+        // is still available
         if (comment.authorId !== input.authorId) {
           throw new Error('Only the author of the comment can edit it.');
         }
 
         const updateOp = { $set: { content: input.content } };
-        const notPendingCommentIdFilter = {
-          id: input.id,
-          pending: { $exists: false }
-        };
-        const reqIdPendingFilter = { 'pending.reqId': context.reqId };
 
-        switch (context.reqType) {
-          case 'vote':
-            await CommentValidation.commentExistsOrFails(comments, input.id);
-            const pendingUpdateObj = await comments
-              .updateOne(
-                notPendingCommentIdFilter,
-                {
-                  $set: {
-                    pending: {
-                      reqId: context.reqId,
-                      type: 'edit-comment'
-                    }
-                  }
-                });
-            if (pendingUpdateObj.matchedCount === 0) {
-              throw new Error(CONCURRENT_UPDATE_ERROR);
-            }
-
-            return true;
-          case undefined:
-            await CommentValidation.commentExistsOrFails(comments, input.id);
-            const updateObj = await comments
-              .updateOne(notPendingCommentIdFilter, updateOp);
-            if (updateObj.matchedCount === 0) {
-              throw new Error(CONCURRENT_UPDATE_ERROR);
-            }
-
-            return updateObj.modifiedCount === 1;
-          case 'commit':
-            await comments.updateOne(
-              reqIdPendingFilter,
-              { ...updateOp, $unset: { pending: '' } });
-
-            return undefined;
-          case 'abort':
-            await comments.updateOne(
-              reqIdPendingFilter, { $unset: { pending: '' } });
-
-            return undefined;
-        }
-
-        return undefined;
+        return comments.updateOne(context, { id: input.id }, updateOp);
       },
 
       deleteComment: async (_root, { input }: { input: DeleteCommentInput },
         context: Context) => {
         const comment = await CommentValidation.commentExistsOrFails(
           comments, input.id);
-
+        // TODO: make this atomic/is this check even needed?
+        // users could use authorization for this
+        // so that the functionality to edit other people's comments
+        // is still available
         if (comment.authorId !== input.authorId) {
           throw new Error('Only the author of the comment can edit it.');
         }
 
-        const notPendingCommentIdFilter = {
-          id: input.id,
-          pending: { $exists: false }
-        };
-        const reqIdPendingFilter = { 'pending.reqId': context.reqId };
-
-        switch (context.reqType) {
-          case 'vote':
-            await CommentValidation.commentExistsOrFails(comments, input.id);
-            const pendingUpdateObj = await comments.updateOne(
-              notPendingCommentIdFilter,
-              {
-                $set: {
-                  pending: {
-                    reqId: context.reqId,
-                    type: 'delete-comment'
-                  }
-                }
-              });
-
-            if (pendingUpdateObj.matchedCount === 0) {
-              throw new Error(CONCURRENT_UPDATE_ERROR);
-            }
-
-            return true;
-          case undefined:
-            await CommentValidation.commentExistsOrFails(comments, input.id);
-            const res = await comments
-              .deleteOne(notPendingCommentIdFilter);
-
-            if (res.deletedCount === 0) {
-              throw new Error(CONCURRENT_UPDATE_ERROR);
-            }
-
-            return true;
-          case 'commit':
-            await comments.deleteOne(reqIdPendingFilter);
-
-            return undefined;
-          case 'abort':
-            await comments.updateOne(
-              reqIdPendingFilter, { $unset: { pending: '' } });
-
-            return undefined;
-        }
-
-        return undefined;
+        return comments.deleteOne(context, { id: input.id });
       }
     }
   };
 }
 
 const commentCliche: ClicheServer = new ClicheServerBuilder('comment')
-  .initDb((db: mongodb.Db, _config: Config): Promise<any> => {
-    const comments: mongodb.Collection<CommentDoc> = db.collection('comments');
+  .initDb((db: ClicheDb, _config: Config): Promise<any> => {
+    const comments: Collection<CommentDoc> = db.collection('comments');
 
     return comments.createIndex({ id: 1 }, { unique: true, sparse: true });
   })
