9cf7b88dc14992bdab3ec44dfda90872a20314bf
Czarina Lao <mcslao@mit.edu>
2019-03-13 20:34:46 -0400

    Update group to use new db layer, but not preserving updateType info yet

--- packages/catalog/group/server/server.ts
+++ packages/catalog/group/server/server.ts
@@ -1,12 +1,12 @@
 import {
   ActionRequestTable,
+  ClicheDb,
   ClicheServer,
   ClicheServerBuilder,
-  CONCURRENT_UPDATE_ERROR,
+  Collection,
   Config,
   Context,
-  getReturnFields,
-  Validation
+  getReturnFields
 } from '@deja-vu/cliche-server';
 import {
   CreateGroupInput,
@@ -16,16 +16,8 @@ import {
 } from './schema';
 
 import * as _ from 'lodash';
-import * as mongodb from 'mongodb';
 import { v4 as uuid } from 'uuid';
 
-
-class GroupValidation {
-  static async groupExistsOrFail(
-    groups: mongodb.Collection<GroupDoc>, id: string): Promise<GroupDoc> {
-    return Validation.existsOrFail(groups, id, 'Group');
-  }
-}
 
 const actionRequestTable: ActionRequestTable = {
   'add-to-group': (extraInfo) => `
@@ -54,7 +46,8 @@ const actionRequestTable: ActionRequestTable = {
       case 'leave':
         return `
           mutation LeaveGroup($groupId: ID!, $id: ID!) {
-            removeMember(groupId: $groupId, id: $id) ${getReturnFields(extraInfo)}
+            removeMember(groupId: $groupId, id: $id)
+              ${getReturnFields(extraInfo)}
           }
         `;
       case 'is-in-group':
@@ -77,86 +70,27 @@ const actionRequestTable: ActionRequestTable = {
       members(input: $input) ${getReturnFields(extraInfo)}
     }
   `
-}
-
-function isPendingCreate(group: GroupDoc | null) {
-  return _.get(group, 'pending.type') === 'create-group';
-}
+};
 
 async function addOrRemoveMember(
-  groups: mongodb.Collection<GroupDoc>, groupId: string, memberId: string,
+  groups: Collection<GroupDoc>, groupId: string, memberId: string,
   updateType: 'add-member' | 'remove-member',
   context: Context): Promise<Boolean> {
   const operation = updateType === 'add-member' ? '$addToSet' : '$pull';
   const updateOp = { [operation]: { memberIds: memberId } };
 
-  const notPendingGroupIdFilter = {
-    id: groupId,
-    pending: { $exists: false }
-  };
-  const reqIdPendingFilter = { 'pending.reqId': context.reqId };
-  switch (context.reqType) {
-    case 'vote':
-      await GroupValidation.groupExistsOrFail(groups, groupId);
-      const pendingUpdateObj = await groups.updateOne(
-        notPendingGroupIdFilter,
-        {
-          $set: {
-            pending: {
-              reqId: context.reqId,
-              type: updateType
-            }
-          }
-        });
-      if (pendingUpdateObj.matchedCount === 0) {
-        throw new Error(CONCURRENT_UPDATE_ERROR);
-      }
-
-      return true;
-    case undefined:
-      await GroupValidation.groupExistsOrFail(groups, groupId);
-      const updateObj = await groups.updateOne(notPendingGroupIdFilter, updateOp);
-      if (updateObj.matchedCount === 0) {
-        throw new Error(CONCURRENT_UPDATE_ERROR);
-      }
-
-      return true;
-    case 'commit':
-      await groups.updateOne(
-        reqIdPendingFilter,
-        { ...updateOp, $unset: { pending: '' } });
-
-      return false;
-    case 'abort':
-      await groups.updateOne(reqIdPendingFilter, { $unset: { pending: '' } });
-
-      return false;
-  }
-
-  return false;
+  return await groups.updateOne(context, { id: groupId }, updateOp);
 }
 
 
-function resolvers(db: mongodb.Db, _config: Config): object {
-  const groups: mongodb.Collection<GroupDoc> = db.collection('groups');
+function resolvers(db: ClicheDb, _config: Config): object {
+  const groups: Collection<GroupDoc> = db.collection('groups');
 
   return {
     Query: {
-      group: async (_root, { id }) => {
-        const group: GroupDoc | null = await groups.findOne({ id: id });
-
-        return isPendingCreate(group) ? null : group;
-      },
+      group: async (_root, { id }) => await groups.findOne({ id: id }),
       members: async (_root, { input }: { input: MembersInput }) => {
-        const noCreateGroupPending = {
-          $or: [
-            { pending: { $exists: false } },
-            { pending: { type: { $ne: 'create-group' } } }
-          ]
-        };
-        const filter = input.inGroupId ?
-          { $and: [{ id: input.inGroupId }, noCreateGroupPending] } :
-          noCreateGroupPending;
+        const filter = input.inGroupId ? { id: input.inGroupId } : {};
 
         const pipelineResults = await groups.aggregate([
           { $match: filter },
@@ -185,19 +119,10 @@ function resolvers(db: mongodb.Db, _config: Config): object {
         return matchingMembers;
       },
       groups: async (_root, { input }: { input: GroupsInput }) => {
-        const noCreateGroupPending = {
-          $or: [
-            { pending: { $exists: false } },
-            { pending: { type: { $ne: 'create-group' } } }
-          ]
-        };
-
         const filter = input.withMemberId ?
-          { $and: [{ memberIds: input.withMemberId }, noCreateGroupPending] } :
-          noCreateGroupPending;
+          { memberIds: input.withMemberId } : {};
 
-        return groups.find(filter)
-          .toArray();
+        return await groups.find(filter);
       }
     },
     Group: {
@@ -211,27 +136,8 @@ function resolvers(db: mongodb.Db, _config: Config): object {
           id: input.id ? input.id : uuid(),
           memberIds: input.initialMemberIds ? input.initialMemberIds : []
         };
-        const reqIdPendingFilter = { 'pending.reqId': context.reqId };
-        switch (context.reqType) {
-          case 'vote':
-            g.pending = { reqId: context.reqId, type: 'create-group' };
-          case undefined:
-            await groups.insertOne(g);
 
-            return g;
-          case 'commit':
-            await groups.updateOne(
-              reqIdPendingFilter,
-              { $unset: { pending: '' } });
-
-            return g;
-          case 'abort':
-            await groups.deleteOne(reqIdPendingFilter);
-
-            return g;
-        }
-
-        return g;
+        return await groups.insertOne(context, g);
       },
       addMember: (_root, { groupId, id }, context: Context) =>
         addOrRemoveMember(groups, groupId, id, 'add-member', context),
@@ -240,60 +146,15 @@ function resolvers(db: mongodb.Db, _config: Config): object {
         addOrRemoveMember(groups, groupId, id, 'remove-member', context),
 
       deleteGroup: async (_root, { id }, context: Context) => {
-        const notPendingGroupIdFilter = {
-          id: id, pending: { $exists: false }
-        };
-        const reqIdPendingFilter = { 'pending.reqId': context.reqId };
-
-        switch (context.reqType) {
-          case 'vote':
-            await GroupValidation.groupExistsOrFail(groups, id);
-            const pendingUpdateObj = await groups.updateOne(
-              notPendingGroupIdFilter,
-              {
-                $set: {
-                  pending: {
-                    reqId: context.reqId,
-                    type: 'delete-group'
-                  }
-                }
-              });
-
-            if (pendingUpdateObj.matchedCount === 0) {
-              throw new Error(CONCURRENT_UPDATE_ERROR);
-            }
-
-            return true;
-          case undefined:
-            await GroupValidation.groupExistsOrFail(groups, id);
-            const res = await groups
-              .deleteOne(notPendingGroupIdFilter);
-
-            if (res.deletedCount === 0) {
-              throw new Error(CONCURRENT_UPDATE_ERROR);
-            }
-
-            return true;
-          case 'commit':
-            await groups.deleteOne(reqIdPendingFilter);
-
-            return undefined;
-          case 'abort':
-            await groups.updateOne(
-              reqIdPendingFilter, { $unset: { pending: '' } });
-
-            return undefined;
-        }
-
-        return undefined;
+        return await groups.deleteOne(context, { id: id });
       }
     }
   };
 }
 
 const groupCliche: ClicheServer = new ClicheServerBuilder('group')
-  .initDb((db: mongodb.Db, _config: Config): Promise<any> => {
-    const groups: mongodb.Collection<GroupDoc> = db.collection('groups');
+  .initDb((db: ClicheDb, _config: Config): Promise<any> => {
+    const groups: Collection<GroupDoc> = db.collection('groups');
 
     return groups.createIndex({ id: 1 }, { unique: true, sparse: true });
   })
