089cd3501ff89ba62b326f1a0e1b9fb16ecc94f0
Czarina Lao <mcslao@mit.edu>
2019-03-15 01:23:06 -0400

    Update task to use new db layer

--- packages/catalog/task/server/server.ts
+++ packages/catalog/task/server/server.ts
@@ -1,30 +1,22 @@
 import {
   ActionRequestTable,
+  ClicheDb,
   ClicheServer,
   ClicheServerBuilder,
-  CONCURRENT_UPDATE_ERROR,
+  Collection,
   Config,
   Context,
-  getReturnFields,
-  Validation
+  getReturnFields
 } from '@deja-vu/cliche-server';
 import * as _ from 'lodash';
-import * as mongodb from 'mongodb';
+import { v4 as uuid } from 'uuid';
 import {
   CreateTaskInput,
   TaskDoc,
   TasksInput,
   UpdateTaskInput
 } from './schema';
-import { v4 as uuid } from 'uuid';
 
-
-class TaskValidation {
-  static async taskExistsOrFail(
-    tasks: mongodb.Collection<TaskDoc>, id: string): Promise<TaskDoc> {
-    return Validation.existsOrFail(tasks, id, 'Task');
-  }
-}
 
 const actionRequestTable: ActionRequestTable = {
   'approve-task': (extraInfo) => `
@@ -69,84 +61,31 @@ const actionRequestTable: ActionRequestTable = {
       default:
         throw new Error('Need to specify extraInfo.action');
     }
-  },
-}
-
-function isPendingCreate(task: TaskDoc | null) {
-  return _.get(task, 'pending.type') === 'create-task';
-}
+  }
+};
 
 async function updateTask(
-  tasks: mongodb.Collection<TaskDoc>, id: string, updateOp: object,
-  updateType: 'update-task' | 'claim-task' | 'complete-task' | 'approve-task',
+  tasks: Collection<TaskDoc>, id: string, updateOp: object,
+  _updateType: 'update-task' | 'claim-task' | 'complete-task' | 'approve-task',
   context: Context): Promise<Boolean> {
-
-  const notPendingTaskFilter = {
-    id,
-    pending: { $exists: false }
-  };
-  const reqIdPendingFilter = { 'pending.reqId': context.reqId };
-  switch (context.reqType) {
-    case 'vote':
-      await TaskValidation.taskExistsOrFail(tasks, id);
-      const pendingUpdateObj = await tasks.updateOne(
-        notPendingTaskFilter,
-        {
-          $set: {
-            pending: {
-              reqId: context.reqId,
-              type: updateType
-            }
-          }
-        });
-      if (pendingUpdateObj.matchedCount === 0) {
-        throw new Error(CONCURRENT_UPDATE_ERROR);
-      }
-
-      return true;
-    case undefined:
-      await TaskValidation.taskExistsOrFail(tasks, id);
-      const updateObj = await tasks.updateOne(notPendingTaskFilter, updateOp);
-      if (updateObj.matchedCount === 0) {
-        throw new Error(CONCURRENT_UPDATE_ERROR);
-      }
-
-      return true;
-    case 'commit':
-      await tasks.updateOne(
-        reqIdPendingFilter,
-        { ...updateOp, $unset: { pending: '' } });
-
-      return true;
-    case 'abort':
-      await tasks.updateOne(reqIdPendingFilter, { $unset: { pending: '' } });
-
-      return true;
-  }
-
-  return true;
+  // TODO: use updateType in db layers
+  return await tasks.updateOne(context, { id }, updateOp);
 }
 
-function resolvers(db: mongodb.Db, _config: Config): object {
-  const tasks: mongodb.Collection<TaskDoc> = db.collection('tasks');
+function resolvers(db: ClicheDb, _config: Config): object {
+  const tasks: Collection<TaskDoc> = db.collection('tasks');
 
   return {
     Query: {
       tasks: async (_root, { input }: { input: TasksInput }) => {
         const filterOp = _.omit(input, ['assigned']);
-        if (input.assigned === false) {
+        if (!input.assigned) {
           filterOp['assigneeId'] = null;
         }
-        filterOp['pending'] = { $in: [ null, { type: { $ne: 'create-task' } } ]};
 
-        return await tasks.find(filterOp)
-          .toArray();
+        return await tasks.find(filterOp);
       },
-      task: async (_root, { id }) => {
-        const task: TaskDoc | null = await tasks.findOne({ id: id });
-
-        return isPendingCreate(task) ? null : task;
-      }
+      task: async (_root, { id }) => await tasks.findOne({ id: id })
     },
     Task: {
       id: (task: TaskDoc) => task.id,
@@ -165,27 +104,8 @@ function resolvers(db: mongodb.Db, _config: Config): object {
           completed: false,
           approved: false
         };
-        const reqIdPendingFilter = { 'pending.reqId': context.reqId };
-        switch (context.reqType) {
-          case 'vote':
-            newTask.pending = { reqId: context.reqId, type: 'create-task' };
-          case undefined:
-            await tasks.insertOne(newTask);
 
-            return newTask;
-          case 'commit':
-            await tasks.updateOne(
-              reqIdPendingFilter,
-              { $unset: { pending: '' } });
-
-            return newTask;
-          case 'abort':
-            await tasks.deleteOne(reqIdPendingFilter);
-
-            return newTask;
-        }
-
-        return newTask;
+        return await tasks.insertOne(context, newTask);
       },
       updateTask: async (
         _root, { input }: { input: UpdateTaskInput }, context: Context) => {
@@ -209,8 +129,8 @@ function resolvers(db: mongodb.Db, _config: Config): object {
 }
 
 const taskCliche: ClicheServer = new ClicheServerBuilder('task')
-  .initDb((db: mongodb.Db, _config: Config): Promise<any> => {
-    const tasks: mongodb.Collection<TaskDoc> = db.collection('tasks');
+  .initDb((db: ClicheDb, _config: Config): Promise<any> => {
+    const tasks: Collection<TaskDoc> = db.collection('tasks');
 
     return tasks.createIndex({ id: 1 }, { unique: true, sparse: true });
   })
