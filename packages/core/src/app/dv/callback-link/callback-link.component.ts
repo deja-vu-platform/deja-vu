d9a628b7aee36a495b48d7e35a119c29baf83d46
Santiago Perez De Rosso <spderosso@gmail.com>
2019-08-02 14:30:16 -0700

    allow params in callback

--- packages/core/src/app/dv/callback-link/callback-link.component.ts
+++ packages/core/src/app/dv/callback-link/callback-link.component.ts
@@ -2,6 +2,15 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 
+/**
+ *  This component is intended to be used with `callback`.
+ *
+ *  When a user clicks on a `callback-link`, `callback-link`
+ *  records the current URL and navigates to the component
+ *  given by `href`. In the href component you are supposed
+ *  to include a `callback` component, which will cause a
+ *  navigation to the saved URL when it runs.
+ */
 @Component({
   selector: 'dv-callback-link',
   templateUrl: './callback-link.component.html'
