798258dcf420cb561015e5946aca0e2abd26a8d6
yunyizhu <yunyizhu@mit.edu>
2019-08-05 19:34:43 -0400

    added a way to format the date in the message

---
+++ packages/core/src/app/dv/format-date/format-date.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component, OnInit, Input, Output, EventEmitter, ElementRef, OnChanges
+} from '@angular/core';
+
+import { RunService } from '../run.service';
+import * as momentImported from 'moment';
+
+/**
+ * Formats an input date string
+ * Both displays the string and outputs the formatted string
+ */
+@Component({
+  selector: 'dv-format-date',
+  templateUrl: './format-date.component.html'
+})
+export class FormatDateComponent implements OnInit, OnChanges {
+  /**
+   * A date string that needs to be formatted
+   */
+  @Input() dateString: string;
+
+  /**
+   * The format method according to
+   * https://devhints.io/moment
+   */
+  @Input() format: string;
+
+  /**
+   * The list of entities after being unioned
+   */
+  @Output() formattedDate = new EventEmitter<string>();
+  _formattedDate = '';
+
+  constructor(
+    private elem: ElementRef, private rs: RunService) {}
+
+  ngOnInit() {
+    this.rs.register(this.elem, this);
+  }
+
+  ngOnChanges() {
+    const moment = momentImported;
+    this._formattedDate = moment(this.dateString)
+      .format(this.format);
+    this.formattedDate.emit(this._formattedDate);
+  }
+}
